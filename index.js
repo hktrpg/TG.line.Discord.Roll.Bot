2000694ec1b6f92e950259905abcfd17df9a9194
zeteticl <cougar.l@hotmail.com>
2016-12-07 19:07:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function parseInput(rplyToken, inputStr) {
         }                   
         //鴨霸獸指令開始於此
 
-        if (trigger.match('鴨霸獸') != null) return randomReply() ;        
+        if (trigger.match('鴨霸獸') != null ||trigger.match('巴獸') != null) return randomReply() ;        
         if (trigger.match('運氣') != null || trigger.match('運勢') != null) return randomLuck(mainMsg) ; //占卜運氣        
         
         if (trigger == 'help') return randomReply() + '\n' + '\

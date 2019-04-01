488ab03d7ee60e39f2a8e0be5a200a5d62f57e87
zeteticl <cougar.l@hotmail.com>
2019-04-02 00:37:16 +0800

    v1.0.3

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ require('fs').readdirSync(__dirname + '/modules/').forEach(function(file) {
     exports[name] = require('./modules/' + file);
   }
 });
+
+/*
+流程解釋
+
+首先這裡會call modules/中的Discord line Telegram 三個檔案
+如果在Heroku 有輸入它們各自的TOKEN 的話
+服務就會各自啓動
+
+Discord line Telegram三套BOT 都會統一呼叫analytics.js
+再由analytics.js 呼叫roll/ 中各個的骰檔
+
+所以基本上,要增加骰組
+都要修改analytics.js 及把新骰組放在Roll中
+
+以上, 有不明可以在GITHUB問我
+
+另外, 使用或參考其中代碼的話, 請保持開源
+感謝
+
+*/

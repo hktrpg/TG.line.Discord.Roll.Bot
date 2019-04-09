bce2045e592aa63d2191253c7add9df849c049e2
zeteticl <cougar.l@hotmail.com>
2019-04-09 18:18:18 +0800

    Q

--- roll/bcdice.js
+++ roll/bcdice.js
@@ -3,16 +3,17 @@ const BCDice = require('bcdice-js').BCDice; // CommonJS
 const bcdice = new BCDice();
 
 function calldice(gameType, message) {
-  bcdice.setGameByTitle(gameType)
-  bcdice.setMessage(message)
-  return bcdice.dice_command()
+    bcdice.setGameByTitle(gameType)
+    bcdice.setMessage(message)
+    return bcdice.dice_command()
 
 }
+/*
 try {
   var resultroll =calldice('DoubleCross', '100d1000+100d1000+100d1000+100d1000+100d1000+100d1000+100d1000')[0 ];
- 
-  console.log(resultroll);
-  switch(expression) {
+
+  //console.log(resultroll);
+  switch(1) {
     case x:
       // code block
       break;
@@ -28,4 +29,4 @@ try {
   console.log(e)
 }
 
-
+*/

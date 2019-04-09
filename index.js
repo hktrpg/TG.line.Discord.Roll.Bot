b44162f44a2f0cdcb521df8fc8681de08ae2eb6c
zeteticl <cougar.l@hotmail.com>
2019-04-09 18:35:51 +0800

    Q

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
+
 require('fs').readdirSync(__dirname + '/modules/').forEach(function (file) {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
     var name = file.replace('.js', '');
     exports[name] = require('./modules/' + file);
   }
 });
-
 
 /*
 流程解釋
--- roll/bcdice.js
+++ roll/bcdice.js
@@ -3,11 +3,56 @@ const BCDice = require('bcdice-js').BCDice; // CommonJS
 const bcdice = new BCDice();
 
 function calldice(gameType, message) {
-    bcdice.setGameByTitle(gameType)
-    bcdice.setMessage(message)
-    return bcdice.dice_command()
+  bcdice.setGameByTitle(gameType)
+  bcdice.setMessage(message)
+  return bcdice.dice_command()
 
 }
+
+
+var rollbase = require('./rollbase.js')
+var rply = { type: 'text' }; // type是必需的,但可以更改
+
+// //////////////////////////////////////
+// ////////////// Sample
+// //////////////////////////////////////
+
+gameName = function () {
+  return 'bcdice'
+}
+
+gameType = function () {
+  return 'bcdice:hktrpg'
+}
+prefixs = function () {
+  return '/^bcd$/i'
+}
+getHelpMessage = function () {
+  return '・bcDice 擲骰\
+  \n使用凍豆骰組\
+  \n'
+}
+initialize = function () {
+  return { default: 'on' }
+}
+
+rollDiceCommand = function (abc) {
+  switch (true) {
+    case /xyz/.test(abc):
+      display("• Matched 'xyz' test");
+      break;
+    case /test/.test(str):
+      display("• Matched 'test' test");
+      break;
+    case /ing/.test(str):
+      display("• Matched 'ing' test");
+      break;
+    default:
+      display("• Didn't match any test");
+      break;
+  }
+}
+
 /*
 try {
   var resultroll =calldice('DoubleCross', '100d1000+100d1000+100d1000+100d1000+100d1000+100d1000+100d1000')[0 ];

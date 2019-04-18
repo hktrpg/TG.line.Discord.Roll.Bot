b735343818fec2a43cf00cb202aa5d37bd3e9ef9
zeteticl <cougar.l@hotmail.com>
2019-04-18 15:24:36 +0800

    Update rollbase.js

--- roll/rollbase.js
+++ roll/rollbase.js
@@ -12,7 +12,7 @@ gameType = function () {
   return 'rollbase:hktrpg'
 }
 prefixs = function () {
-  return /(^\d+d\d+)|(^\d+)|(^\(\d+d\d+)|(^\(\d+)/i
+  return /(^\d+d\d+)|(^\d+$)|(^\(\d+d\d+)|(^\(\d+)/i
 }
 getHelpMessage = function () {
   return "【基本擲骰】1d100\
@@ -69,50 +69,50 @@ try {
   function FunnyDice(diceSided) {
     return Math.floor((Math.random() * diceSided)) // 猜拳，從0開始
   }
-  
-    function BuildDiceCal(inputStr) {
-  
-      // 首先判斷是否是誤啟動（檢查是否有符合骰子格式）
-      if (inputStr.toLowerCase().match(/\d+d\d+/) == null) return undefined
-  
-      // 排除小數點
-      if (inputStr.toString().match(/\./) != null) return undefined
-  
-      // 先定義要輸出的Str
-      let finalStr = ''
-  
-      // 一般單次擲骰
-      let DiceToRoll = inputStr.toString().toLowerCase()
-      if (DiceToRoll.match('d') == null) return undefined
-  
-      // 寫出算式
-      let equation = DiceToRoll
-      while (equation.match(/\d+d\d+/) != null) {
-        let tempMatch = equation.match(/\d+d\d+/)
-        if (tempMatch.toString().split('d')[0] > 200) return '欸欸，不支援200D以上擲骰；哪個時候會骰到兩百次以上？想被淨灘嗎？'
-        if (tempMatch.toString().split('d')[1] == 1 || tempMatch.toString().split('d')[1] > 500) return '不支援D1和超過D500的擲骰；想被淨灘嗎？'
-        equation = equation.replace(/\d+d\d+/, BuildRollDice(tempMatch))
-      }
-  
-      // 計算算式
-      let answer = eval(equation.toString())
-      finalStr = equation + ' = ' + answer
-  
-      return finalStr
+
+  function BuildDiceCal(inputStr) {
+
+    // 首先判斷是否是誤啟動（檢查是否有符合骰子格式）
+    if (inputStr.toLowerCase().match(/\d+d\d+/) == null) return undefined
+
+    // 排除小數點
+    if (inputStr.toString().match(/\./) != null) return undefined
+
+    // 先定義要輸出的Str
+    let finalStr = ''
+
+    // 一般單次擲骰
+    let DiceToRoll = inputStr.toString().toLowerCase()
+    if (DiceToRoll.match('d') == null) return undefined
+
+    // 寫出算式
+    let equation = DiceToRoll
+    while (equation.match(/\d+d\d+/) != null) {
+      let tempMatch = equation.match(/\d+d\d+/)
+      if (tempMatch.toString().split('d')[0] > 200) return '欸欸，不支援200D以上擲骰；哪個時候會骰到兩百次以上？想被淨灘嗎？'
+      if (tempMatch.toString().split('d')[1] == 1 || tempMatch.toString().split('d')[1] > 500) return '不支援D1和超過D500的擲骰；想被淨灘嗎？'
+      equation = equation.replace(/\d+d\d+/, BuildRollDice(tempMatch))
     }
-  
-    function BuildRollDice(inputStr) {
-      // 先把inputStr變成字串（不知道為什麼非這樣不可）
-      let comStr = inputStr.toString().toLowerCase()
-      let finalStr = '('
-  
-      for (let i = 1; i <= comStr.split('d')[0]; i++) {
-        finalStr = finalStr + Dice(comStr.split('d')[1]) + '+'
-      }
-  
-      finalStr = finalStr.substring(0, finalStr.length - 1) + ')'
-      return finalStr
+
+    // 計算算式
+    let answer = eval(equation.toString())
+    finalStr = equation + ' = ' + answer
+
+    return finalStr
+  }
+
+  function BuildRollDice(inputStr) {
+    // 先把inputStr變成字串（不知道為什麼非這樣不可）
+    let comStr = inputStr.toString().toLowerCase()
+    let finalStr = '('
+
+    for (let i = 1; i <= comStr.split('d')[0]; i++) {
+      finalStr = finalStr + Dice(comStr.split('d')[1]) + '+'
     }
+
+    finalStr = finalStr.substring(0, finalStr.length - 1) + ')'
+    return finalStr
+  }
   // //////////////////////////////////////
   // ////////////// 普通ROLL
   // //////////////////////////////////////

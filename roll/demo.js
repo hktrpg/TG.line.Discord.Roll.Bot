2a1d20551815df3750cd6409c88bce0baba2b9c6
zeteticl <cougar.l@hotmail.com>
2019-04-27 01:33:16 +0800

    Update demo.js

--- roll/demo.js
+++ roll/demo.js
@@ -15,6 +15,7 @@ prefixs = function () {
     //[mainMSG[0]的prefixs,mainMSG[1]的prefixs,   <---這裡是一對  
     //mainMSG[0]的prefixs,mainMSG[1]的prefixs  ]  <---這裡是一對
     //如前面是 /^1$/ig, 後面是/^1D100$/ig, 即 prefixs 變成 1 1D100 
+    ///^(?=.*he)(?!.*da).*$/ig
     return [/^Demo$/i,/^啊$/i]
 }
 getHelpMessage = function () {

16e4f40f50078a7ee3b601f54da15a2b68793fe5
zeteticl <cougar.l@hotmail.com>
2019-04-27 03:14:39 +0800

    update help

--- roll/help.js
+++ roll/help.js
@@ -43,6 +43,7 @@ getHelpMessage = function () {
 \n 會輸出）2d6+1：攻撃  9[6+3]+1 = 10\
 \n 如上面一樣,在骰子數字後方隔空白位打字,可以進行發言。\
 \n 5 3D6 ：	分別骰出5次3d6 (最多三十次)\
+\n ((2d6+1)*2)-5/2>=10 ：	支援括號加減乘除及大於小於(>,<,>=,<=)計算\
 \n  \
 \n Line版 https://line.me/R/ti/p/svMLqy9Mik\
 \n Discord版 https://discordapp.com/oauth2/authorize?&client_id=544462904037081138&scope=bot&permissions=8\
@@ -72,9 +73,9 @@ rollDiceCommand = function (inputStr, mainMsg) {
 			for (i = 0; i < Object.keys(exports).length; i++) {
 				if (exports[Object.keys(exports)[i]] && exports[Object.keys(exports)[i]].gameName)
 					rply.text += "\n" +
-						i + ": " +
-						//exports[Object.keys(exports)[i]].gameName() +
-						exports[Object.keys(exports)[i]].getHelpMessage()
+					i + ": " +
+					//exports[Object.keys(exports)[i]].gameName() +
+					exports[Object.keys(exports)[i]].getHelpMessage()
 			}
 			return rply;
 
@@ -87,8 +88,8 @@ rollDiceCommand = function (inputStr, mainMsg) {
 			for (i = 0; i < Object.keys(exports).length; i++) {
 				if (exports[Object.keys(exports)[i]] && exports[Object.keys(exports)[i]].gameName)
 					rply.text += "\n" +
-						i + ": " +
-						exports[Object.keys(exports)[i]].gameName()
+					i + ": " +
+					exports[Object.keys(exports)[i]].gameName()
 			}
 
 			//console.log('case: ', rply)
@@ -132,200 +133,200 @@ function linehelp() {
 		"template": {
 			"type": "carousel",
 			"columns": [{
-				//Array of columns
-				//Max: 10
-				"title": "《基本擲骰系統》",
-				//Title Max: 40 characters
-				"text": "【擲骰BOT】v1.0.3 指令包括1D100, 5B10 ,5U10 8 9",
-				//Message text
-				//Max: 120 characters (no image or title)
-				//Max: 60 characters (message with an image or title)
-				"actions": [{
-					"type": "message",
-					"label": "1d100擲骰範例",
-					"text": "5 1d100 骰出5次1D100"
-				},
-				{
-					"type": "message",
-					"label": "5B10擲骰範例",
-					"text": "5B10 9 不加總的擲骰,計算其中有多少粒大於9"
-				},
-				{
-					"type": "message",
-					"label": "5U10 8 9擲骰範例",
-					"text": "5U10 8 9 每骰出一粒8會有一粒獎勵骰及計算有多少粒大於9"
-				}
-					//Action when tapped
-					//Max: 3
-				]
-			}, {
-				"title": "《COC 6 7版 擲骰系統》",
-				"text": "指令包括 6版ccb, 7版cc, cc(n)1~2, cc6版創角, cc7版創角, coc7角色背景",
-				"actions": [{
-					"type": "message",
-					"label": "6版擲骰 技能80",
-					"text": "ccb 80 擒抱!"
-				},
-				{
-					"type": "message",
-					"label": "7版擲骰 cc((n)1-2) d ",
-					"text": "ccn2 80 7版擲骰 n代表懲罰 1-2數量 d 目標值"
-				},
-				{
-					"type": "message",
-					"label": "cc7版創角 50歲",
-					"text": "cc7版創角 50"
-				}
+					//Array of columns
+					//Max: 10
+					"title": "《基本擲骰系統》",
+					//Title Max: 40 characters
+					"text": "【擲骰BOT】v1.0.3 指令包括1D100, 5B10 ,5U10 8 9",
+					//Message text
+					//Max: 120 characters (no image or title)
+					//Max: 60 characters (message with an image or title)
+					"actions": [{
+							"type": "message",
+							"label": "1d100擲骰範例",
+							"text": "5 1d100 骰出5次1D100"
+						},
+						{
+							"type": "message",
+							"label": "5B10擲骰範例",
+							"text": "5B10 9 不加總的擲骰,計算其中有多少粒大於9"
+						},
+						{
+							"type": "message",
+							"label": "5U10 8 9擲骰範例",
+							"text": "5U10 8 9 每骰出一粒8會有一粒獎勵骰及計算有多少粒大於9"
+						}
+						//Action when tapped
+						//Max: 3
+					]
+				}, {
+					"title": "《COC 6 7版 擲骰系統》",
+					"text": "指令包括 6版ccb, 7版cc, cc(n)1~2, cc6版創角, cc7版創角, coc7角色背景",
+					"actions": [{
+							"type": "message",
+							"label": "6版擲骰 技能80",
+							"text": "ccb 80 擒抱!"
+						},
+						{
+							"type": "message",
+							"label": "7版擲骰 cc((n)1-2) d ",
+							"text": "ccn2 80 7版擲骰 n代表懲罰 1-2數量 d 目標值"
+						},
+						{
+							"type": "message",
+							"label": "cc7版創角 50歲",
+							"text": "cc7版創角 50"
+						}
 
-				]
-			},
-			{
-				"title": "《COC7版 擲骰系統》",
-				"text": "指令包括 即時型瘋狂 總結型瘋狂",
-				"actions": [{
-					"type": "message",
-					"label": "即時型瘋狂Real Time",
-					"text": "ccrt"
+					]
 				},
 				{
-					"type": "message",
-					"label": "總結型瘋狂Summary",
-					"text": "ccsu"
-				},
-				{
-					"type": "message",
-					"label": "成長或增強檢定",
-					"text": "DP 50 騎馬Development Phase | 成長檢定 45 頭槌 | 幕間成長 40 單車"
-				}
+					"title": "《COC7版 擲骰系統》",
+					"text": "指令包括 即時型瘋狂 總結型瘋狂",
+					"actions": [{
+							"type": "message",
+							"label": "即時型瘋狂Real Time",
+							"text": "ccrt"
+						},
+						{
+							"type": "message",
+							"label": "總結型瘋狂Summary",
+							"text": "ccsu"
+						},
+						{
+							"type": "message",
+							"label": "成長或增強檢定",
+							"text": "DP 50 騎馬Development Phase | 成長檢定 45 頭槌 | 幕間成長 40 單車"
+						}
 
-				]
-			},
-			{
-				"title": "《其他系統01》",
-				"text": "NC死靈年代記之永遠的後日談, WoD黑暗世界",
-				"actions": [{
-					"type": "message",
-					"label": "1NC 擲骰範例",
-					"text": "2NC"
+					]
 				},
 				{
-					"type": "message",
-					"label": "NM 依戀擲骰範例",
-					"text": "nm"
-				},
-				{
-					"type": "message",
-					"label": "WOD擲骰範例",
-					"text": "5wd8 投擲5次D10 每有一粒大於8,得到一粒獎勵骰"
-				}
+					"title": "《其他系統01》",
+					"text": "NC死靈年代記之永遠的後日談, WoD黑暗世界",
+					"actions": [{
+							"type": "message",
+							"label": "1NC 擲骰範例",
+							"text": "2NC"
+						},
+						{
+							"type": "message",
+							"label": "NM 依戀擲骰範例",
+							"text": "nm"
+						},
+						{
+							"type": "message",
+							"label": "WOD擲骰範例",
+							"text": "5wd8 投擲5次D10 每有一粒大於8,得到一粒獎勵骰"
+						}
 
-				]
-			},
-			{
-				"title": "《其他系統02》",
-				"text": "DX3雙重十字 nDXc+m n=骰數 c=暴擊值 m=其他修正",
-				"actions": [{
-					"type": "message",
-					"label": "DX3 擲骰範例",
-					"text": "5DX8-1+8"
+					]
 				},
 				{
-					"type": "message",
-					"label": "DX3 擲骰範例",
-					"text": "8DX+1"
-				},
-				{
-					"type": "message",
-					"label": "DX3 擲骰範例",
-					"text": "2DX5-1"
-				}
+					"title": "《其他系統02》",
+					"text": "DX3雙重十字 nDXc+m n=骰數 c=暴擊值 m=其他修正",
+					"actions": [{
+							"type": "message",
+							"label": "DX3 擲骰範例",
+							"text": "5DX8-1+8"
+						},
+						{
+							"type": "message",
+							"label": "DX3 擲骰範例",
+							"text": "8DX+1"
+						},
+						{
+							"type": "message",
+							"label": "DX3 擲骰範例",
+							"text": "2DX5-1"
+						}
 
-				]
-			},
-			{
-				"title": "《其他系統03》",
-				"text": "SW2.0 劍世界 KKn+m-m@c$d$+xGF n=骰數 c=暴擊值 m=其他修正 d=固定值 x=增加值",
-				"actions": [{
-					"type": "message",
-					"label": "SW2.0 擲骰範例",
-					"text": "KK28-8+1"
+					]
 				},
 				{
-					"type": "message",
-					"label": "SW2.0 擲骰範例",
-					"text": "KK050+8-1@8"
-				},
-				{
-					"type": "message",
-					"label": "SW2.0 擲骰範例",
-					"text": "KK050+8@8$9gf"
-				}
+					"title": "《其他系統03》",
+					"text": "SW2.0 劍世界 KKn+m-m@c$d$+xGF n=骰數 c=暴擊值 m=其他修正 d=固定值 x=增加值",
+					"actions": [{
+							"type": "message",
+							"label": "SW2.0 擲骰範例",
+							"text": "KK28-8+1"
+						},
+						{
+							"type": "message",
+							"label": "SW2.0 擲骰範例",
+							"text": "KK050+8-1@8"
+						},
+						{
+							"type": "message",
+							"label": "SW2.0 擲骰範例",
+							"text": "KK050+8@8$9gf"
+						}
 
-				]
-			},
-			{
-				"title": "《附加功能》",
-				"text": "排序及隨機功能,D66, D66s",
-				"actions": [{
-					"type": "message",
-					"label": "排序功能範例",
-					"text": "交換禮物排序 A君 C君 F君 G君"
+					]
 				},
 				{
-					"type": "message",
-					"label": "隨機功能範例",
-					"text": "隨機收到聖誕禮物數 1 2 3 >4"
-				},
-				{
-					"type": "message",
-					"label": "D66s 骰出D66 小至大",
-					"text": "D66s 骰出D66 小至大"
-				}
+					"title": "《附加功能》",
+					"text": "排序及隨機功能,D66, D66s",
+					"actions": [{
+							"type": "message",
+							"label": "排序功能範例",
+							"text": "交換禮物排序 A君 C君 F君 G君"
+						},
+						{
+							"type": "message",
+							"label": "隨機功能範例",
+							"text": "隨機收到聖誕禮物數 1 2 3 >4"
+						},
+						{
+							"type": "message",
+							"label": "D66s 骰出D66 小至大",
+							"text": "D66s 骰出D66 小至大"
+						}
 
-				]
-			},
-			{
-				"title": "《附加功能2》",
-				"text": "塔羅牌,運氣占卜,死亡FLAG. ",
-				"actions": [{
-					"type": "message",
-					"label": "塔羅占卜",
-					"text": "單張塔羅/大十字塔羅/每日塔羅牌/時間tarot 單張,大十字,每日及時間必須放頭"
+					]
 				},
 				{
-					"type": "message",
-					"label": "死亡FLAG",
-					"text": "立Flag/死亡flag"
-				},
-				{
-					"type": "message",
-					"label": "運氣占卜",
-					"text": "來吧運勢!"
-				}
+					"title": "《附加功能2》",
+					"text": "塔羅牌,運氣占卜,死亡FLAG. ",
+					"actions": [{
+							"type": "message",
+							"label": "塔羅占卜",
+							"text": "單張塔羅/大十字塔羅/每日塔羅牌/時間tarot 單張,大十字,每日及時間必須放頭"
+						},
+						{
+							"type": "message",
+							"label": "死亡FLAG",
+							"text": "立Flag/死亡flag"
+						},
+						{
+							"type": "message",
+							"label": "運氣占卜",
+							"text": "來吧運勢!"
+						}
 
-				]
-			},
-			{
-				"title": "《介紹》",
-				"text": "Discord版,Telegram版及源碼 ",
-				"actions": [{
-					"type": "message",
-					"label": "Discord版",
-					"text": "https://discordapp.com/oauth2/authorize?&client_id=544462904037081138&scope=bot&permissions=8"
+					]
 				},
 				{
-					"type": "message",
-					"label": "Telegram版",
-					"text": "http://t.me/hktrpg_bot"
-				},
-				{
-					"type": "message",
-					"label": "骰子機器人原始碼",
-					"text": "https://github.com/hktrpg/TG.line.Discord.Roll.Bot"
-				}
+					"title": "《介紹》",
+					"text": "Discord版,Telegram版及源碼 ",
+					"actions": [{
+							"type": "message",
+							"label": "Discord版",
+							"text": "https://discordapp.com/oauth2/authorize?&client_id=544462904037081138&scope=bot&permissions=8"
+						},
+						{
+							"type": "message",
+							"label": "Telegram版",
+							"text": "http://t.me/hktrpg_bot"
+						},
+						{
+							"type": "message",
+							"label": "骰子機器人原始碼",
+							"text": "https://github.com/hktrpg/TG.line.Discord.Roll.Bot"
+						}
 
-				]
-			}
+					]
+				}
 			]
 		}
 	};

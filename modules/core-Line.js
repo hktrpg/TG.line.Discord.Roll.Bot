4a7df57d6e1c52df5d74e0c595d6c594960e4d54
zeteticl <cougar.l@hotmail.com>
2019-05-15 03:25:46 +0800

    Update core-Line.js

--- modules/core-Line.js
+++ modules/core-Line.js
@@ -99,7 +99,7 @@ if (process.env.LINE_CHANNEL_ACCESSTOKEN) {
 			var mainMsg = event.message.text.match(msgSplitor); // 定義輸入字串
 		if (mainMsg && mainMsg[0])
 			var trigger = mainMsg[0].toString().toLowerCase(); // 指定啟動詞在第一個詞&把大階強制轉成細階
-		console.log(getDisplayName(event));
+		console.log(profile.displayName);
 		// 訊息來到後, 會自動跳到analytics.js進行骰組分析
 		// 如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
 

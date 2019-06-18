031b14b18768358005bab3691855aa8bbb87b5b4
zeteticl <cougar.l@hotmail.com>
2019-06-18 15:44:35 +0800

    update

--- roll/demo.js
+++ roll/demo.js
@@ -16,7 +16,7 @@ prefixs = function () {
     //mainMSG[0]的prefixs,mainMSG[1]的prefixs  ]  <---這裡是一對
     //如前面是 /^1$/ig, 後面是/^1D100$/ig, 即 prefixs 變成 1 1D100 
     ///^(?=.*he)(?!.*da).*$/ig
-    return [/^Demo$/i,/^啊$/i]
+    return [/^Demo$/i, /^啊$/i]
 }
 getHelpMessage = function () {
     return "【示範】" + "\
@@ -30,6 +30,9 @@ initialize = function () {
 rollDiceCommand = function (inputStr, mainMsg) {
     rply.text = '';
     switch (true) {
+        case /^help$/i.test(mainMsg[1]):
+            rply.text = this.getHelpMessage();
+            return rply;
         case /^\d+$/i.test(mainMsg[0]):
             rply.text = 'Demo' + mainMsg[1]
             return rply;

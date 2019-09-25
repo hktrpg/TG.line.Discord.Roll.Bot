92832db88f5948b6a73d03506eeb46c2e5c91fa5
zeteticl <cougar.l@hotmail.com>
2019-09-25 12:12:37 +0800

    Update z_DDR_darkRollingToGM.js

--- roll/z_DDR_darkRollingToGM.js
+++ roll/z_DDR_darkRollingToGM.js
@@ -8,33 +8,29 @@ try {
     records.get('randomAns', (msgs) => {
         rply.randomAnsfunction = msgs
     })
-    records.get('randomAnsAllgroup', (msgs) => {
-        rply.randomAnsAllgroup = msgs
-    })
     gameName = function () {
-        return '(公測中)自定義回應功能 .ra(p)(次數) (add del show 自定關鍵字)'
+        return '(公測中)暗骰GM功能 .drgm  (add del show 自定關鍵字)'
     }
     gameType = function () {
-        return 'randomAns:hktrpg'
+        return 'trpgDarkRolling:hktrpg'
     }
     prefixs = function () {
-        return [/(^[.]ra(\d+|p|p\d+|)$)/ig, ]
+        return [/(^[.]drgm$)/ig,]
     }
     getHelpMessage = function () {
-        return "【自定義回應功能】" + "\
-        \n 這是根據關鍵字來隨機抽選功能,只要符合內容,以後就會隨機抽選\
-        \n 例如輸入 .ra add 九大陣營 守序善良 (...太長省略) 中立邪惡 混亂邪惡 \
-        \n 再輸入.ra 九大陣營  就會輸出 九大陣營中其中一個\
-        \n 如果輸入.ra3 九大陣營  就會輸出 3次九大陣營\
-        \n 如果輸入.ra3 九大陣營 天干 地支 就會輸出 3次九大陣營 天干 地支\
-        \n add 後面第一個是關鍵字, 可以是漢字,數字和英文或emoji\
-        \n P.S.如果沒立即生效 用.ra show 刷新一下\
-    \n 輸入.ra add (關鍵字) (選項1) (選項2) (選項3)即可增加關鍵字\
-    \n 輸入.ra show 顯示所有關鍵字\
-    \n 輸入.ra del(編號)或all 即可刪除\
-    \n 輸入.ra(次數,最多30次) (關鍵字1)(關鍵字2)(關鍵字n) 即可隨機抽選 \
-    \n 如使用輸入.rap 會變成全服版,全服可看, 可用add show功能 \
-    \n 例如輸入 .rap10 聖晶石召喚 即可十連抽了 \
+        return "【暗骰GM功能】.drgm .dr .ddr .dddr" + "\
+        \n 這是讓你可以私骰GM的功能,\
+        \n 例如輸入 .ddr cc 80 鬥毆 \
+        \n 就會把結果私訊GM及自己\
+        \n 例如輸入 .dddr cc 80 鬥毆 \
+        \n 就會把結果只私訊GM\
+        \n P.S.如果沒立即生效 用.drgm show 刷新一下\
+    \n 輸入.drgm addgm 即可成為GM\
+    \n 輸入.drgm show 顯示所有GM\
+    \n 輸入.drgm del(編號)或all 即可刪除\
+    \n 輸入.dr  (指令) 私訊自己 \
+    \n 輸入.ddr (指令) 私訊GM及自己\
+    \n 輸入.dddr(指令) 私訊GM\
     \n "
     }
     initialize = function () {
@@ -231,7 +227,7 @@ try {
                 //
                 //增加
                 //
-                
+
                 let checkifsamenamegroup = 0
                 if (rply.randomAnsAllgroup && mainMsg[2] && mainMsg[3] && mainMsg[4])
                     for (var i = 0; i < rply.randomAnsAllgroup.length; i++) {

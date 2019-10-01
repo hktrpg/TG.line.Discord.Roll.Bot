5167fc952f1ac47d2322eb60a286afd05a9f9bcf
zeteticl <cougar.l@hotmail.com>
2019-10-01 10:56:52 +0800

    improve help

--- roll/help.js
+++ roll/help.js
@@ -97,7 +97,7 @@ rollDiceCommand = function (inputStr, mainMsg, groupid, userid, userrole, botnam
 					exports[Object.keys(exports)[i]].gameName()
 			}
 			if (botname == "Line")
-				rply.text += "因為Line的機制, 如擲骰時並無顯示用家名字, 請到下列網址,和機器人任意說一句話. \n https://line.me/R/ti/p/svMLqy9Mik"
+				rply.text += "\n因為Line的機制, 如擲骰時並無顯示用家名字, 請到下列網址,和機器人任意說一句話,成為好友. \n https://line.me/R/ti/p/svMLqy9Mik"
 
 			//console.log('case: ', rply)
 			return rply;

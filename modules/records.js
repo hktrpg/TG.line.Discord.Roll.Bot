4c9166a9b19b94a3b3094b987ae04c53678e276d
zeteticl <cougar.l@hotmail.com>
2019-11-15 15:28:16 +0800

    Update records.js

--- modules/records.js
+++ modules/records.js
@@ -439,7 +439,7 @@ class Records extends EventEmitter {
                 //在這群組查詢等級時的回應
                 Switch: msg.Switch,
                 //是否啓動功能 config 1X 則1
-                Hidden: msg.Hidden,
+                Hidden: msg.Hidden
                 //是否顯示升級語 config X1 則1
                 //trpgLevelSystemfunction: msg.trpgLevelSystemfunction
             }
@@ -494,7 +494,7 @@ class Records extends EventEmitter {
                 //在這群組查詢等級時的回應
                 Switch: msg.Switch,
                 //是否啓動功能 config 1X 則1
-                Hidden: msg.Hidden,
+                Hidden: msg.Hidden
                 //是否顯示升級語 config X1 則1
                 //trpgLevelSystemfunction: msg.trpgLevelSystemfunction
             }
@@ -546,20 +546,65 @@ class Records extends EventEmitter {
     */
     settrpgSaveLogfunctionRealTime(dbbase, msgA, msg, callback) {
         schema[dbbase].findOneAndUpdate({
-
             groupid: msgA.groupid
-
         }, {
             $set: {
-                //LevelUpWord: msg.LevelUpWord
-                //在這群組升級時的升級語
-                //RankWord: msg.RankWord
-                //在這群組查詢等級時的回應
-                //Switch: msg.Switch,
-                //是否啓動功能 config 1X 則1
-                //Hidden: msg.Hidden,
-                //是否顯示升級語 config X1 則1
-                trpgLevelSystemfunction: msg
+                //實時資料 使用SET
+                RealTimeRollingLogfunction: msg
+                //中途紀錄資料 使用PUSH 每天紀錄一次
+                // RollingLogfunction: msg,
+                //擲骰的結果紀錄
+                //Sided: msg
+            }
+        }, {
+            //   setDefaultsOnInsert: true
+        }, (err, doc) => {
+            if (err) {
+                console.log(err);
+                console.log("Something wrong when updating data!");
+            } else {
+                callback();
+                // console.log('DONE?')
+            }
+            // return JSON.stringify(doc).toString();
+        });
+    }
+    pushtrpgSaveLogfunction(dbbase, msgA, msg, callback) {
+        schema[dbbase].findOneAndUpdate({
+            groupid: msgA.groupid
+        }, {
+            $push: {
+                //實時資料 使用SET
+                // RealTimeRollingLogfunction: msg,
+                //中途紀錄資料 使用PUSH 每天紀錄一次
+                RollingLogfunction: msg
+                //擲骰的結果紀錄
+                //Sided: msg
+            }
+        }, {
+            //   setDefaultsOnInsert: true
+        }, (err, doc) => {
+            if (err) {
+                console.log(err);
+                console.log("Something wrong when updating data!");
+            } else {
+                callback();
+                // console.log('DONE?')
+            }
+            // return JSON.stringify(doc).toString();
+        });
+    }
+    settrpgSaveLogfunctionRoll(dbbase, msgA, msg, callback) {
+        schema[dbbase].findOneAndUpdate({
+            groupid: msgA.groupid
+        }, {
+            $set: {
+                //實時資料 使用SET
+                // RealTimeRollingLogfunction: msg,
+                //中途紀錄資料 使用PUSH 每天紀錄一次
+                //RollingLogfunction: msg,
+                //擲骰的結果紀錄
+                Sided: msg
             }
         }, {
             //   setDefaultsOnInsert: true

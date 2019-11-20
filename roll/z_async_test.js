1f4aa7d93f8fc5eadd4f6680893b491f770eb793
zeteticl <cougar.l@hotmail.com>
2019-11-20 18:59:11 +0800

    UPDATE

--- roll/z_async_test.js
+++ roll/z_async_test.js
@@ -9,7 +9,7 @@ const wiki = require('wikijs').default;
 const timer = require('timer');
 const translate = require('@vitalets/google-translate-api');
 gameName = function () {
-	return 'Wiki查詢/即時翻譯'
+	return '(公測中)Wiki查詢/即時翻譯 .wiki .tran'
 }
 
 gameType = function () {
@@ -21,8 +21,19 @@ prefixs = function () {
 }
 
 getHelpMessage = function () {
-	return "ONLINE\
-		\n "
+	return "【Wiki查詢/即時翻譯】.wiki .tran .tran.(目標語言) (內容)\
+		\n 新增即時網上Wiki查詢及即時翻譯功能\
+		\n Wiki功能: .wiki (條目)  \
+		\n EG: .wiki BATMAN  \
+		\n 即時翻譯功能: .Tran (內容)  \
+		\n 預設翻譯成正體中文\
+		\n EG: .tran BATMAN  \
+		\n 可翻譯成其他語言: .tran.(語系) (內容)\
+		\n EG: .tran.ja BATMAN  \
+		\n 常用語言代碼: 英=en, 簡=zh-cn, 德=de, 日=ja\
+		\n 常用代碼可用中文字, 例子: .tran.英\n.tran.日\n.tran.de \
+		\n 語系代碼 https://github.com/vitalets/google-translate-api/blob/master/languages.js\
+		"
 }
 initialize = function () {
 	return rply;
@@ -58,12 +69,12 @@ rollDiceCommand = async function (inputStr, mainMsg, groupid, userid, userrole, 
 		case /\S+/.test(mainMsg[1]) && /^[.]tran[.]\S+$/.test(mainMsg[0]):
 			let lang = /.tran.(\S+)/;
 			let test = mainMsg[0].match(lang)
-			rply.text = await translate(inputStr.replace(mainMsg[0], ""), { to: test[1] }).then(res => {
+			rply.text = await translate(inputStr.replace(mainMsg[0], ""), { to: test[1].replace("簡中", "zh-CN").replace("簡體", "zh-CN").replace(/zh-cn/ig, "zh-CN").replace("英", "en").replace("簡", "zh-CN").replace("德", "de").replace("日", "ja") }).then(res => {
 				//console.log(res.from.language.iso);
 				return res.text
 			}).catch(err => {
 				console.log(err.message)
-				return err.message;
+				return err.message + "\n常用語言代碼: 英=en, 簡=zh-cn, 德=de, 日=ja\n例子: .tran.英\n.tran.日\n.tran.de";
 			});
 			return rply;
 		default:

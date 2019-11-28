0bde671133849d42933af5c54b123dc3833d0575
zeteticl <cougar.l@hotmail.com>
2019-11-28 12:14:32 +0800

    Revert "Update core-Line.js"
    This reverts commit 027280ab4347b998d757b0a462bfb82ef83f2816.

--- modules/core-Line.js
+++ modules/core-Line.js
@@ -11,13 +11,12 @@ if (process.env.LINE_CHANNEL_ACCESSTOKEN) {
 
 	// create LINE SDK config from env variables
 	var bot = line({
-		channelId: process.env.CHANNEL_ID,
 		channelSecret: process.env.LINE_CHANNEL_SECRET,
 		channelAccessToken: process.env.LINE_CHANNEL_ACCESSTOKEN
 	});
 
 	// create LINE SDK client
-
+	
 	const app = express();
 
 	const linebotParser = bot.parser();

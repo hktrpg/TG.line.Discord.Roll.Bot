b2fa4b77100223d66a363841ccbedd2869042d5a
zeteticl <cougar.l@hotmail.com>
2020-04-27 23:26:26 +0800

    Update WN.js

--- roll/WN.js
+++ roll/WN.js
@@ -5,24 +5,22 @@ var rply = {
 };
 
 gameName = function () {
-    return 'Demo'
+    return '【魔女狩獵之夜】.wn xDn(+-)y'
 }
 
 gameType = function () {
-    return 'Demo:hktrpg'
+    return 'witch-hunting-night:hktrpg'
 }
 prefixs = function () {
-    //[mainMSG[0]的prefixs,mainMSG[1]的prefixs,   <---這裡是一對  
-    //mainMSG[0]的prefixs,mainMSG[1]的prefixs  ]  <---這裡是一對
-    //如前面是 /^1$/ig, 後面是/^1D100$/ig, 即 prefixs 變成 1 1D100 
-    ///^(?=.*he)(?!.*da).*$/ig
-    return [/^.$/i, /^啊$/i]
+    return [/^.wn$/i, /^\d/i]
 }
 getHelpMessage = function () {
     return "【魔女狩獵之夜】" + "\
-	\n  .wn xDn(+-)y  x\
-\n.wn x@n(+-)y\
-		\n "
+    \n  .wn xD(D)n(+-y)  x骰池 n罪業值 y調整值 \
+    \n有第二個D會使用成功數減去失敗數為最後的成功數(可負數)\
+\可以沒有D\
+    \n.wn x@n(+-)y(D) 魔改版 x骰池 n罪業值 y調整值\
+		\n 魔改版 少於等於罪業值為失敗"
 }
 initialize = function () {
     return rply;
@@ -34,18 +32,30 @@ rollDiceCommand = async function (inputStr, mainMsg, groupid, userid, userrole, 
         case /^help$/i.test(mainMsg[1]) || !mainMsg[1]:
             rply.text = this.getHelpMessage();
             return rply;
-        case /^\d+$/i.test(mainMsg[0]):
-            rply.text = 'Demo' + mainMsg[1]
-            return rply;
-        case /^(?![\s\S])/.test(mainMsg[0] || ''):
-            rply.text = 'Demo'
+        case /^\d/i.test(mainMsg[1]):
+            rply.text = WN(mainMsg[1])
             return rply;
         default:
             break;
     }
 }
 
+function WN(message) {
+    //x@n(+-y)(D)
+    //xD(D)n(+-y)
 
+    //5d6
+    //5d6d
+    //5dd6
+    //5dd
+    //5d6+5-5
+
+    //5@6
+    //5@5d
+    //5@5-5
+    //5@6-5D
+    let regex = /^(\d+)(@|D)/ig
+}
 module.exports = {
     rollDiceCommand: rollDiceCommand,
     initialize: initialize,

9b3625d8c82402fec1557c7706c89bbf35894930
zeteticl <cougar.l@hotmail.com>
2020-08-25 15:40:26 +0800

    Update session.js

---
+++ modules/session.js
@@ -0,0 +1,57 @@
+"use strict";
+/** 
+bothelp
+
+請問有什麼可以幫你? 
+請輸入你想查詢的項目數字.
+-------
+1.查詢版本及公告(xxxx時間更新)
+2.查詢trpg 不同系統擲骰指令
+3.查詢trpg 輔助工具
+4.查詢系統工具
+5.查詢趣味功能
+6.查詢hktrpg 不同平台連結
+7.意見提供
+-----
+輸入 1 或 bothelp 公告 或 bothelp 版本
+【HKTRPG擲骰BOT】" + version 
+及公告
+------
+輸入 2 或 bothelp Dice
+0: 【進階擲骰】 .ca (計算)|D66(sn)|5B10 Dx|5U10 x y|.int x y
+2: 【克蘇魯神話】 cc cc(n)1~2 ccb ccrt ccsu .dp .cc7build .cc6build .cc7bg
+3: 【朱の孤塔】 .al (nALxp)
+4: 【神我狩】 .kk (ET RT NT KT MTx)
+5: 【迷宮王國】 .mk (nMK+m 及各種表)
+6: 【亞俠必死的冒險】 .ss (nR>=x[y,z,c] SRx+y FumbleT)
+7: 【忍神】 .sg (ST FT ET等各種表)
+8: 【歌風】 .UK (nUK nUK@c or nUKc)
+9: 【魔女狩獵之夜】.wn xDn+-y
+10: 【DX2nd,3rd】 .dx (xDX+y@c ET)
+11: 【命運Fate】 .4df(m|-)(加值)
+12: 【永遠的後日談】 .nc (NM xNC+m xNA+m)
+13: 【劍世界2.5】.sw (Kx Gr FT TT)
+14: 【WOD黑暗世界】.xWDy
+15: 【貓貓鬼差】.kc xDy z
+------
+輸入 1 或 bothelp 公告 或 bothelp 版本
+【HKTRPG擲骰BOT】" + version 
+及公告
+------
+輸入 1 或 bothelp 公告 或 bothelp 版本
+【HKTRPG擲骰BOT】" + version 
+及公告
+------
+輸入 1 或 bothelp 公告 或 bothelp 版本
+【HKTRPG擲骰BOT】" + version 
+及公告
+------
+輸入 1 或 bothelp 公告 或 bothelp 版本
+【HKTRPG擲骰BOT】" + version 
+及公告
+------
+輸入 1 或 bothelp 公告 或 bothelp 版本
+【HKTRPG擲骰BOT】" + version 
+及公告
+------
+ */

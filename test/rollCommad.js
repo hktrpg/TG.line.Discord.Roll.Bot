3465872696686c7608a686c463935c4d544bd5a3
Zeteticl <zetet@dasd.com>
2020-09-07 20:26:38 +0800

    update

--- test/rollCommad.js
+++
@@ -1,8 +0,0 @@
-const parseInput = require('../modules/core-analytics').parseInput;
-describe('測試所有指令輸出有反應', () => {
-    it('測試 1D10', () => {
-        if (parseInput('1D10')) {
-            throw new Error("兩數相加結果不為兩數和");
-        }
-    });
-});

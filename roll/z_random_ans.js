ad54ef310410756d11ac62c4a0c0421f3850fc5a
zeteticl <cougar.l@hotmail.com>
2020-09-23 18:30:00 +0800

    Update z_random_ans.js

--- roll/z_random_ans.js
+++ roll/z_random_ans.js
@@ -379,6 +379,60 @@ var rollDiceCommand = async function ({
     }
 }
 
+
+async function findGp(groupid, userid, displayname, displaynameDiscord, membercount) {
+    if (!process.env.mongoURL || !Object.keys(level.trpgLevelSystemfunction).length) {
+        return;
+    }
+
+    //1. 檢查GROUP ID 有沒有開啓CONFIG 功能 1
+    let userInfo = {};
+    let gpInfo = exports.z_Level_system.initialize().trpgLevelSystemfunction.find(e => e.groupid == groupid);
+    if (!gpInfo || gpInfo.Switch != 1) return;
+    if (gpInfo.trpgLevelSystemfunction) {
+        userInfo = gpInfo.trpgLevelSystemfunction.find(e => e.userid == userid)
+    }
+    userInfo.name = displaynameDiscord || displayname || '無名'
+    return await returnTheLevelWord(gpInfo, userInfo, membercount);
+    //6 / 7 * LVL * (2 * LVL * LVL + 30 * LVL + 100)
+}
+
+async function returnTheLevelWord(gpInfo, userInfo, membercount) {
+    let username = userInfo.name;
+    let userlevel = userInfo.Level;
+    let userexp = userInfo.EXP;
+    let usermember_count = membercount || gpInfo.trpgLevelSystemfunction.length;
+    let userRanking = await ranking(userInfo.userid, gpInfo.trpgLevelSystemfunction);
+    let userRankingPer = Math.ceil(userRanking / usermember_count * 10000) / 100 + '%';
+    let userTitle = await exports.z_Level_system.checkTitle(userlevel, gpInfo.Title);
+    let tempUPWord = gpInfo.LevelUpWord || "恭喜 {user.name}《{user.title}》，你的克蘇魯神話知識現在是 {user.level}點了！\n現在排名是{server.member_count}人中的第{user.Ranking}名！";
+    return tempUPWord.replace(/{user.name}/ig, username).replace(/{user.level}/ig, userlevel).replace(/{user.exp}/ig, userexp).replace(/{user.Ranking}/ig, userRanking).replace(/{user.RankingPer}/ig, userRankingPer).replace(/{server.member_count}/ig, usermember_count).replace(/{user.title}/ig, userTitle);
+}
+
+async function ranking(who, data) {
+    let array = [];
+    let answer = "0";
+    for (let key in data) {
+        await array.push(data[key]);
+    }
+    array.sort(function (a, b) {
+        return b.EXP - a.EXP;
+    });
+    let rank = 1;
+    for (let i = 0; i < array.length; i++) {
+        if (i > 0 && array[i].EXP < array[i - 1].EXP) {
+            rank++;
+        }
+        array[i].rank = rank;
+    }
+    for (let b = 0; b < array.length; b++) {
+        if (array[b].userid == who)
+            answer = b + 1;
+
+    }
+    return answer;
+}
+
 async function replaceAsync(str, regex, asyncFn) {
     const promises = [];
     str.replace(regex, (match, ...args) => {
@@ -401,6 +455,7 @@ async function replacer(first, second) {
             if (!temp || !temp[1] || !temp[2]) return;
             return await rollbase.DiceINT(temp[1], temp[2]);
         case /^allgp.name$/i.test(second):
+
             // * {allgp.name} <---隨機全GP其中一人名字
             break;
         case /^allgp.title$/i.test(second):

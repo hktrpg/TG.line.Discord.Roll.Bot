827f5ac9e34f76198c28b9703fbf77bf3d141a48
zeteticl <cougar.l@hotmail.com>
2020-09-24 14:57:23 +0800

    update

--- modules/level.js
+++ modules/level.js
@@ -1,5 +1,5 @@
 if (!process.env.mongoURL) return;
-const oneMinuts = 60000;
+const oneMinuts = (process.env.DEBUG) ? 1 : 60000;
 //60000 一分鐘多久可以升級及增加經驗
 exports.rollbase = require('../roll/rollbase');
 exports.z_Level_system = require('../roll/z_Level_system');

71fd440ffb6eedafc24de446628a662b5a3f95b2
zeteticl <cougar.l@hotmail.com>
2020-10-21 17:52:50 +0800

    VIP EDIT

--- roll/export.js
+++ roll/export.js
@@ -5,12 +5,18 @@ var gameName = function () {
     return '【Discord 頻道輸出工具】'
 }
 const VIP = require('../modules/veryImportantPerson');
-const limitArr = [20, 20, 20, 30, 30, 99, 99, 99];
+const limitArr = [1, 20, 40, 40, 40, 99, 99, 99];
 /**
- *  因為資源限制，
- *  每個guild 5分鐘可以使用一次,
- *  每個ACC可以一星期
+ * 因為資源限制，
+ * 每個guild 5分鐘可以使用一次,
+ * 每個ACC可以一星期一次
+ * 
+ *  
+ * 升級的話, 個人一星期20次
+ * 只有一分鐘限制
+ * 
  */
+const schema = require('../modules/core-schema.js');
 const fs = require('fs').promises;
 const moment = require('moment-timezone');
 const CryptoJS = require("crypto-js");
@@ -19,19 +25,25 @@ var gameType = function () {
 }
 const dir = __dirname + '/../tmp/';
 var prefixs = function () {
-    //[mainMSG[0]的prefixs,mainMSG[1]的prefixs,   <---這裡是一對  
-    //mainMSG[0]的prefixs,mainMSG[1]的prefixs  ]  <---這裡是一對
-    //如前面是 /^1$/ig, 後面是/^1D100$/ig, 即 prefixs 變成 1 1D100 
-    ///^(?=.*he)(?!.*da).*$/ig
     return [{
         first: /^[.]discord$/i,
         second: null
     }]
 }
 var getHelpMessage = function () {
-    return "【示範】" + "\n\
-只是一個Demo的第一行\n\
-只是一個Demo末行"
+    return "【聊天紀錄】" + "\n\
+.discord html 可以輸出有分析功能的聊天紀錄\n\
+.discord txt 可以輸出純文字的聊天紀錄\n\
+需要使用者及rollbot 都有閱讀頻道聊天紀錄的權限\n\
+然後會私訊你紀錄\n\
+\n\
+因為資源限制，\n\
+每個群組 5分鐘可以使用一次,\n\
+每個ACC可以一星期使用一次\n\
+\n\
+經patreon解鎖功能的話可以一星期使用20次以上，\n\
+及可以一分鐘使用一次。\n\
+https://www.patreon.com/HKTRPG"
 }
 var initialize = function () {
     return variables;
@@ -56,15 +68,18 @@ var rollDiceCommand = async function ({
     let totalSize = 0;
     let newRawDate = [];
     let newValue = "";
+    let lv, limit, check;
     let channelName = discordMessage.channel.name || '';
-    var date = new Date;
-    var seconds = date.getSeconds();
-    var minutes = date.getMinutes();
-    var hour = date.getHours();
-    var tempA = channelid + '_' + hour + minutes + seconds;
+    let date = new Date;
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hour = date.getHours();
+    let tempA = channelid + '_' + hour + minutes + seconds;
+    let permission, hasReadPermission;
+
     if (groupid) {
-        var permission = (discordMessage.channel && discordMessage.channel.permissionsFor(discordClient.user).has("READ_MESSAGE_HISTORY")) || (discordMessage.member && discordMessage.member.hasPermission("ADMINISTRATOR"));
-        var hasReadPermission = discordMessage.channel.permissionsFor(discordMessage.guild.me).has("READ_MESSAGE_HISTORY") || discordMessage.guild.me.hasPermission("ADMINISTRATOR");
+        permission = (discordMessage.channel && discordMessage.channel.permissionsFor(discordClient.user).has("READ_MESSAGE_HISTORY")) || (discordMessage.member && discordMessage.member.hasPermission("ADMINISTRATOR"));
+        hasReadPermission = discordMessage.channel.permissionsFor(discordMessage.guild.me).has("READ_MESSAGE_HISTORY") || discordMessage.guild.me.hasPermission("ADMINISTRATOR");
     }
 
     function replacer(first, second) {
@@ -78,7 +93,7 @@ var rollDiceCommand = async function ({
             rply.text = this.getHelpMessage();
             return rply;
         case /^html$/i.test(mainMsg[1]):
-            if (!groupid) {
+            if (!channelid || !groupid) {
                 rply.text = "這是頻道功能，需要在頻道上使用。"
                 return rply;
             }
@@ -94,7 +109,16 @@ var rollDiceCommand = async function ({
                 rply.text = "這是Discord限定功能"
                 return rply;
             }
-            if (!channelid || !groupid) return;
+            lv = await VIP.viplevelCheckUser(userid);
+            limit = limitArr[lv];
+            check = await schema.characterCard.find({
+                id: userid
+            });
+            if (check.length >= limit) {
+                rply.text = '你每星期下載聊天紀錄的上限為' + limit + '次' + '\n支援及解鎖上限 https://www.patreon.com/HKTRPG\n或自組服務器\n源代碼  http://bit.ly/HKTRPG_GITHUB';
+                return rply
+            }
+
             C = await discordClient.channels.fetch(channelid);
             discordMessage.channel.send("<@" + userid + '>\n' + ' 請等等，HKTRPG現在開始努力處理，需要一點時間');
             M = await lots_of_messages_getter(C);

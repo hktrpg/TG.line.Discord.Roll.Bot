bf58649b27283408bf8b3a3eec1a3557665c740d
FX097 <FX00097@gmail.com>
2021-01-20 16:52:00 +0800

    hotfix for line

--- modules/core-www.js
+++ modules/core-www.js
@@ -1,5 +1,5 @@
 "use strict";
-if (!process.env.LINE_CHANNEL_ACCESSTOKEN || !process.env.mongoURL || !process.env.SITE) {
+if (!process.env.LINE_CHANNEL_ACCESSTOKEN || !process.env.mongoURL) {
     return;
 }
 const {

7e4860f7b3671181ffdd598192fdba385b82d513
zeteticl <cougar.l@hotmail.com>
2021-07-02 16:24:49 +0800

    x

--- roll/z_event.js
+++ roll/z_event.js
@@ -654,81 +654,153 @@ module.exports = {
 
 
 
-/*
-以個人為單位, 一張咭可以在不同的群組使用
-.char add 的輸入格式,用來增建事件
-.char add name[Sad]~
-state[HP:5/5;MP:3/3;SAN:50/99;護甲:6]~
-roll[投擲:cc 80 投擲;空手鬥毆: cc [[50 +{hp}]]]~
-notes[筆記:SAD;心靈支柱: 特質]~
+/**
+ * TODO:
+ * 1. .evt (event)系統設計
+經由新增的事件 可以增加減少EXP
+功能及設計列表
+1. 舉報不良項目, 有幾個個舉報, 自動隱藏
+3. 
+進入事件的方法
+輸入 .evt event ->   即 進入 隨機事件, 消耗5EN
+輸入 .evt (事件名稱) ->   即 進入 指定事件, 消耗15EN
 
-// state 可以進行增減
-// notes 文字筆記
-// roll 擲骰指令
+EN= 20+LV
+每5分鐘回複1點EN
 
-如果沒有名字 會更新修正正在USE的事件
-但沒有的話,  就會出錯
-============
+得知事件名稱的方法，別人告知 或 經隨機事件知道名字
 
+4. 
+事件效果
+1. 沒有事發生
+2. 直接增加X點經驗(X分鐘內)
+3. 直接減少X點經驗(X分鐘內)
+4. 停止得到經驗(X分鐘內)
+5. 分發X經驗給整個CHANNEL中的X人
+6. 停止得到經驗(X分鐘內)並每次減少發言減少X經驗
+7. 吸收對方X點經驗
+8. 對方得到經驗值 X 倍(X分鐘內)
+9. 從整個CHANNEL 的X人吸收X點經驗
 
-===
-.char use 使用事件
-.char use sad
-會自動使用名叫Sad 的事件
-====
-.char nonuse
-.char use
-會取消在此群組使用事件
+5. 
+設計事件的好處
+能夠吸收對方消耗的en 作為自己的exp
 
-====
-.char delete  事件
-.char delete Sad
-刪除事件
+6.
+設計方式
+輸入 .evt add 天命
+你被雷打中 得到{exp}點真氣  2  (直接增加X點經驗)
+你掉下山中 頭破血流，損失{exp}點真氣  3  (直接減少X點經驗)
+今天風平浪靜 1 (無事發生)
 
-====
+可以有3+(ROUNDDOWN 設計者LV/10)  項結果
+由設計者自己設定
+一個事件由以下三項組成
+事件名稱，事件內容及設定事件結果 
 
-顯示SHOW 功能:
+7. 
+限制
+A. 一個事件中，正面選項要比負面選項多
+B. 事件效果隨著設計者LV 而開發
+如: 效果1-3 LV0-10 可用
+4 需要LV11-20LV
+5 需要LV21-30
+C. 一個事件中，不可以全部正面效果
+D. 一個事件可用的總EN 為(10+LV)，負面事件消耗X點EN
 
-.ch show (顯示 名字 state 和roll)
-.ch shows  (顯示 名字 state,notes 和roll)
-.ch show notes (顯示 名字 和notes)
+8.
+變數X 普通為
+設計者LV , 
+使用者LV, 
+設計者LV 與使用者LV 的相差,
+負面效果的程度(即如果一個事件中有負面效果，那正面效果會增加)
+ * 
+ * 
+ * A) .evt event / .evt 指定名字   - roll/event.js  (檢查有沒有開EXP功能)
+ * B) 沒有則RETURN，
+ *      有->傳送GP ID, USER ID, 名字 到 MODULES/EVENT.JS
+ *      取得MONGOOSE資料 ->進行  (randomEvent)
+ *       i)   抽選整個列表      
+ *      ii)   抽選指定列表
+ * C)   從該列表中抽選一個結果 (randomEvent)
+ * D)   得到結果後，進行 該運算 (event)
+ *      1/8個結果   -> (expChange)
+ * E)   得到結果，修改MONGOOSE (editExp)
+ * F)   翻回文字結果到使用者(roll/event.js)
+ * 
+ * 
+ * 
+ */
+async function randomEvent({
+    freeMode,
+    eventName
+}) {
+    //free mode = 從整個列表抽選
+    if (freeMode) {
+        const target = await schema.eventList.find({});
+        if (!target.length) return;
+        const targetEvent = target[exports.rollbase.Dice(target.length) - 1]
+        return targetEvent[exports.rollbase.Dice(targetEvent.length) - 1]
+    } else if (eventName) {
+        const target = await schema.eventList.findOne({
+            title: eventName
+        });
+        if (!target) return;
+        return target[exports.rollbase.Dice(target.length) - 1]
+    } else return;
 
+}
 
-角色名字
-HP: 5/5 MP: 3/3 SAN: 50/90 護甲: 6
--------
-投擲: cc 80 投擲
-空手: cc 50
--------
-筆記: SAD
-心靈支柱: 特質
+async function event(key, needExp, eventLV, myLV, eventNeg) {
+    let random
+    switch (key) {
+        case 2:
+            //   2. 直接增加X點經驗
+            //100之一 ->50之一 * 1.0X ( 相差LV)% *1.0X(負面級數)^(幾個負面) 
+            random = exports.rollbase.DiceINT(needExp / 100, needExp / 50)
+            random *= (eventLV ^ 2 - myLV) > 0 ? ((eventLV ^ 2 - myLV) / 100 + 1) : 1;
+            random *= (eventNeg / 100 + 1)
+            return random;
+        case 3:
+            // 3. 直接減少X點經驗
+            //100之一 ->50之一 * 1.0X ( 相差LV)% *1.0X(負面級數)^(幾個負面) 
+            random = exports.rollbase.DiceINT(needExp / 200, needExp / 50)
+            random *= (eventLV - myLV ^ 2) > 0 ? ((eventLV - myLV ^ 2) / 100 + 1) : 1;
+            random *= (1 - eventNeg / 100)
+            return random;
 
-======
+        case 4:
+            //   4. 停止得到經驗(X分鐘內)
+            random = eventLV;
 
-功能 使用事件的state 和notes
+            break;
+        case 5:
+            //  5. 分發X經驗給整個CHANNEL中的X人
+            random = exports.rollbase.DiceINT(needExp / 50, needExp / 20)
+            random *= (eventLV ^ 2 - myLV) > 0 ? ((eventLV ^ 2 - myLV) / 100 + 1) : 1;
+            random *= (eventNeg / 100 + 1)
+            return random;
+        case 6:
+            //  6. 停止得到經驗(X分鐘內) 並每次減少發言減少X經驗
+            random = eventLV;
+            break;
+        case 7:
+            //  7. 吸收對方X點經驗
 
-.ch set HP  10 直接把現在值變成10
-.ch set HP  10/20 直接把現在值變成10 最大值變成20
+            break;
+        case 8:
+            //  8. 對方得到經驗值 X 倍(X分鐘內)
+            random = exports.rollbase.DiceINT(needExp / 200, needExp / 50)
+            random *= (eventLV - myLV ^ 2) > 0 ? ((eventLV - myLV ^ 2) / 100 + 1) : 1;
+            random *= (1 - eventNeg / 100)
+            break;
+        case 9:
+            //  9. 從整個CHANNEL 的X人吸收X點經驗
 
+            break;
 
-
-.ch HP MP 顯示該內容
-HP 5/5 MP 3/3
-
-.ch HP -5 如果HP是State 自動減5
-.ch HP +5  如果HP是State 自動加5 如果是
-
-
-
-============
-.ch 輸出指令
-.ch  投擲
-cc 80 投擲
-在指令中可以加上 +{HP} -{san}
-在結果中會進行運算。
-
-
-======
-
-
-*/
+        default:
+            //     1. 沒有事發生
+            break;
+    }
+}

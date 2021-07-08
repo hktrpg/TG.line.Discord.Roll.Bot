ac89aab14d35d18240b2851183bd452afcfcf642
zeteticl <cougar.l@hotmail.com>
2021-07-08 16:32:03 +0800

    Update core-plurk.js

--- modules/core-plurk.js
+++ modules/core-plurk.js
@@ -9,80 +9,70 @@ const Plurk_Client = new PlurkClient(process.env.PLURK_APPKEY, process.env.PLURK
 exports.analytics = require('./core-analytics');
 Plurk_Client.request('Users/me')
     .then(profile => {
-        console.log('RUN', profile.full_name);
+        console.log(profile.full_name);
         plurkID = profile.id;
     })
     .catch(err => console.error(err));
 
 
 
-(function loop() {
-    setTimeout(function () {
-        console.log('START?')
-        Plurk_Client.startComet();
+Plurk_Client.startComet();
 
-        Plurk_Client.on('new_plurk', async response => {
-            if (response.type != 'new_plurk') return;
-            if (response.limited_to && response.limited_to.length == 1 && response.limited_to[0] == 0) return;
-            let message = response.content_raw;
-            if (!message) return;
-            let mainMsg = message.match(msgSplitor); // 定義輸入字串
-            if (mainMsg.length > 1) {
-                if (!mainMsg[0].match(/@HKTRPG/i)) return;
-                mainMsg.shift();
-            }
-            else return;
+Plurk_Client.on('new_plurk', async response => {
+    if (response.type != 'new_plurk') return;
+    if (response.limited_to && response.limited_to.length == 1 && response.limited_to[0] == 0) return;
+    let message = response.content_raw;
+    if (!message) return;
+    let mainMsg = message.match(msgSplitor); // 定義輸入字串
+    if (mainMsg.length > 1) {
+        if (!mainMsg[0].match(/@HKTRPG/i)) return;
+        mainMsg.shift();
+    }
+    else return;
 
-            // 訊息來到後, 會自動跳到analytics.js進行骰組分析
-            // 如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
-            let rplyVal = await exports.analytics.parseInput({
-                inputStr: mainMsg.join(' '),
-                botname: "Plurk"
-            });
-            if (rplyVal && rplyVal.text) {
-                return await sendMessage(response.plurk_id, rplyVal.text);
-            }
-        });
+    // 訊息來到後, 會自動跳到analytics.js進行骰組分析
+    // 如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
+    let rplyVal = await exports.analytics.parseInput({
+        inputStr: mainMsg.join(' '),
+        botname: "Plurk"
+    });
+    if (rplyVal && rplyVal.text) {
+        return await sendMessage(response.plurk_id, rplyVal.text);
+    }
+});
 
-        Plurk_Client.on('new_response', async response => {
-            if (response.user[plurkID]) return;
-            if (response.type != 'new_response') return;
-            if (response.limited_to && response.limited_to.length == 1 && response.limited_to[0] == 0) return;
-            let message = response.response.content_raw;
-            if (!message) return;
-            let mainMsg = message.match(msgSplitor); // 定義輸入字串
+Plurk_Client.on('new_response', async response => {
+    if (response.user[plurkID]) return;
+    if (response.type != 'new_response') return;
+    if (response.limited_to && response.limited_to.length == 1 && response.limited_to[0] == 0) return;
+    let message = response.response.content_raw;
+    if (!message) return;
+    let mainMsg = message.match(msgSplitor); // 定義輸入字串
 
-            if (mainMsg.length > 1) {
-                if (!mainMsg[0].match(/@HKTRPG/i)) return;
-                mainMsg.shift();
-            }
-            else return;
+    if (mainMsg.length > 1) {
+        if (!mainMsg[0].match(/@HKTRPG/i)) return;
+        mainMsg.shift();
+    }
+    else return;
 
-            // 訊息來到後, 會自動跳到analytics.js進行骰組分析
-            // 如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
-            let rplyVal = await exports.analytics.parseInput({
-                inputStr: mainMsg.join(' '),
-                botname: "Plurk"
-            });
+    // 訊息來到後, 會自動跳到analytics.js進行骰組分析
+    // 如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
+    let rplyVal = await exports.analytics.parseInput({
+        inputStr: mainMsg.join(' '),
+        botname: "Plurk"
+    });
 
-            if (rplyVal && rplyVal.text) {
-                let displayName = '';
-                for (var i in response.user) {
-                    if (i == response.response.user_id)
-                        displayName = `${response.user[i].display_name}\n`
+    if (rplyVal && rplyVal.text) {
+        let displayName = '';
+        for (var i in response.user) {
+            if (i == response.response.user_id)
+                displayName = `${response.user[i].display_name}\n`
 
-                }
-                rplyVal.text = `${displayName}${rplyVal.text}`
-                await sendMessage(response.plurk.plurk_id, rplyVal.text);
-            }
-        })
-        loop()
-    }, 60 * 1000 * 30); //60 * 1000 milsec
-}());
-
-
-
-
+        }
+        rplyVal.text = `${displayName}${rplyVal.text}`
+        await sendMessage(response.plurk.plurk_id, rplyVal.text);
+    }
+})
 
 async function sendMessage(response, rplyVal) {
     try {

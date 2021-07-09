784e22c1043625684ee20a8961a8b0ff0d1774b4
Lin1009 <76976991+Lin1009@users.noreply.github.com>
2021-07-10 06:02:01 +0800

    Update z_trpgDatabase.js

--- roll/z_trpgDatabase.js
+++ roll/z_trpgDatabase.js
@@ -14,7 +14,7 @@ records.get('trpgDatabaseAllgroup', (msgs) => {
 const VIP = require('../modules/veryImportantPerson');
 const limitArr = [30, 200, 200, 300, 300, 300, 300, 300];
 var gameName = function () {
-    return '(公測中)資料庫功能 (公共|)背包 (登記 刪除 顯示 自定關鍵字)'
+    return '(公測中)資料庫功能 (公共)背包 (登記 刪除 顯示 自定關鍵字)'
 }
 var gameType = function () {
     return 'funny:trpgDatabase:hktrpg'

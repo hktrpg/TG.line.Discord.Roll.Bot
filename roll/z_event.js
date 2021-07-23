cc39d0bd046d4b27830ffd4b8c79968d9797f678
zeteticl <cougar.l@hotmail.com>
2021-07-23 15:38:21 +0800

    10分鐘回複一點EN.

--- roll/z_event.js
+++ roll/z_event.js
@@ -399,7 +399,7 @@ var rollDiceCommand = async function ({
                 if (!eventMember.energy) {
                     eventMember.energy = maxLv + 20;
                 }
-                let EnergyRecover = Math.round(((new Date(Date.now()) - new Date(eventMember.lastActiveAt))) / 1000 * 60 * 5 / 100000);
+                let EnergyRecover = Math.round(((new Date(Date.now()) - new Date(eventMember.lastActiveAt))) / 1000 * 60 * 10 / 100000);
                 eventMember.energy = Math.min(maxLv + 20, EnergyRecover + eventMember.energy);
                 eventMember.lastActiveAt = new Date(Date.now());
                 (debugMode) ? eventMember.energy = 99 : null;

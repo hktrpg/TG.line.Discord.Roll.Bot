bac763ec0bf109d0004010e9d6a28cadfa6e7ea1
zeteticl <cougar.l@hotmail.com>
2021-08-24 18:18:58 +0800

    Update core-Line.js

--- modules/core-Line.js
+++ modules/core-Line.js
@@ -28,8 +28,8 @@ app.post('/', line.middleware(config), (req, res) => {
 	Promise
 		.all(req.body.events.map(handleEvent))
 		.then((result) => res.json(result))
-		.catch((err) => {
-			console.error(err);
+		.catch(() => {
+			//	console.error(err);
 			res.status(500).end();
 		});
 });

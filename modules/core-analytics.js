4c6486ec01e285778154e1908a0ee0a1b7f8eac7
zeteticl <cougar.l@hotmail.com>
2021-10-22 17:13:31 +0800

    Update core-analytics.js

--- modules/core-analytics.js
+++ modules/core-analytics.js
@@ -224,6 +224,7 @@ async function stateText() {
 	text += '\n 網頁版總擲骰次數: ' + state.WWWCountRoll;
 	text += '\n 使用經驗值功能的群組: ' + await schema.trpgLevelSystem.countDocuments({ Switch: '1' });
 	text += '\n 已新增的角色卡: ' + await schema.characterCard.countDocuments({});
+	text += '\n HKTRPG使用者數量: ' + await schema.firstTimeMessage.countDocuments({});
 	text += '\n 擲骰系統使用的隨機方式: random-js nodeCrypto';
 	return text;
 }

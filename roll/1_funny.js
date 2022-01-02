fccd1fd6e93fa0b8c53c563b8855b9340db419d4
Sad <cougar.l@hotmail.com>
2022-01-02 14:48:33 +0800

    fix 靈簽沒有結果

--- roll/1_funny.js
+++ roll/1_funny.js
@@ -807,8 +807,9 @@ async function fatchDaily(url) {
 		let reply = '';
 		const response = await fetch(url);
 		const json = await response.json();
+		console.log('json', json)
 		if (json.text) reply = json.text;
-		if (json.data && json.data.text) reply = `${json.data.title || ''}\n${json.data.text || ''}\n${json.data.image || ''}`;
+		if (json.data && (json.data.text || json.data.image || json.data.title)) reply = `${json.data.title ? json.data.title + '\n' : ''}${json.data.text ? json.data.text + '\n' : ''}${json.data.image || ''}`;
 		return chineseConv.tify(reply) || '沒有結果，請檢查內容'
 	} catch (error) {
 		console.error(error);

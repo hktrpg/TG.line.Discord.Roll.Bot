4f8d0e235937ec5a109363bb2a4c0d1211039fa7
Sad <cougar.l@hotmail.com>
2022-01-27 04:38:18 +0800

    update

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -55,7 +55,7 @@ const client = new Client({
 		cachePresences: false
 	 */
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES,
-	Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS]
+	Intents.FLAGS.DIRECT_MESSAGE_REACTIONS]
 });
 
 const msgSplitor = (/\S+/ig);

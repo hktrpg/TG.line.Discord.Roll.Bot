fc046c719988367e4013475379e123ac58ffcd88
Lin1009 <76976991+Lin1009@users.noreply.github.com>
2022-03-01 05:26:50 +0800

    Update core-www.js

--- modules/core-www.js
+++ modules/core-www.js
@@ -7,10 +7,9 @@ const {
     RateLimiterMemory
 } = require('rate-limiter-flexible');
 const msgSplitor = (/\S+/ig)
-const schema = require('./schema.js');
+const schema = require('./core-schema.js');
 const privateKey = (process.env.KEY_PRIKEY) ? process.env.KEY_PRIKEY : null;
 const certificate = (process.env.KEY_CERT) ? process.env.KEY_CERT : null;
-const APIswitch = (process.env.API) ? process.env.API : null;
 const ca = (process.env.KEY_CA) ? process.env.KEY_CA : null;
 const isMaster = (process.env.MASTER) ? process.env.MASTER : null;
 const www = require('./core-Line').app;
@@ -32,10 +31,6 @@ const rateLimiterCard = new RateLimiterMemory({
     duration: 60, // per second
 });
 
-const rateLimiterApi = new RateLimiterMemory({
-    points: 10000, // 5 points
-    duration: 10, // per second
-});
 
 async function read() {
     if (!privateKey) return;
@@ -46,7 +41,7 @@ async function read() {
             ca: (fs.readFileSync(ca)) ? fs.readFileSync(ca) : null
         };
     } catch (error) {
-        console.error('error of key')
+        console.log('error of key')
     }
 }
 
@@ -54,17 +49,19 @@ async function read() {
     read()
 })();
 var server;
-createWebServer();
-process.on('uncaughtException', (warning) => {
-    console.warn(warning.name); // Print the warning name
-    console.warn(warning.message); // Print the warning message
-    var clock = setTimeout(createWebServer, 60000 * 5);
-});
+if (!options.key) {
+    server = require('http').createServer(www);
+    console.log('http server');
+} else {
+    server = require('https').createServer(options, www);
+    console.log('https server');
+}
+
 const io = require('socket.io')(server);
 const records = require('./records.js');
 const port = process.env.PORT || 20721;
 var channelKeyword = '';
-exports.analytics = require('./analytics');
+exports.analytics = require('./core-analytics');
 
 // 加入線上人數計數
 let onlineCount = 0;
@@ -72,51 +69,6 @@ let onlineCount = 0;
 www.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
 });
-www.get('/api', async (req, res) => {
-    if (!APIswitch || await limitRaterApi(req.ip)) return;
-
-    if (
-        !req || !req.query || !req.query.msg
-    ) {
-        res.writeHead(200, { 'Content-type': 'application/json' })
-        res.end('{"message":"welcome to HKTRPG API.\\n To use, please enter the content in query: msg \\n like /api?msg=1d100\\n command bothelp for tutorials."}')
-        return;
-    }
-
-    var ip = req.headers['x-forwarded-for'] ||
-        req.socket.remoteAddress ||
-        null;
-    if (ip && await limitRaterApi(ip)) return;
-    let rplyVal = {}
-    var mainMsg = req.query.msg.match(msgSplitor); // 定義輸入字串
-    if (mainMsg && mainMsg[0])
-        var trigger = mainMsg[0].toString().toLowerCase(); // 指定啟動詞在第一個詞&把大階強制轉成細階
-
-    // 訊息來到後, 會自動跳到analytics.js進行骰組分析
-    // 如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
-    if (channelKeyword != '' && trigger == channelKeyword.toString().toLowerCase()) {
-        rplyVal = await exports.analytics.parseInput({
-            inputStr: mainMsg.join(' '),
-            botname: "Api"
-        })
-
-    } else {
-        if (channelKeyword == '') {
-            rplyVal = await exports.analytics.parseInput({
-                inputStr: mainMsg.join(' '),
-                botname: "Api"
-            })
-        }
-    }
-
-    if (!rplyVal || !rplyVal.text) rplyVal.text = null;
-    res.writeHead(200, { 'Content-type': 'application/json' })
-    res.end(`{"message":"${jsonEscape(rplyVal.text)}"}`)
-    return;
-
-
-});
-
 www.get('/card', (req, res) => {
     res.sendFile(process.cwd() + '/views/characterCard.html');
 });
@@ -141,12 +93,12 @@ io.on('connection', async (socket) => {
             userName: message.userName,
             password: SHA(message.userPassword)
         }
-        let doc = await schema.accountPW.findOne(filter).catch(error => console.error('www #144 mongoDB error: ', error.name, error.reson));
+        let doc = await schema.accountPW.findOne(filter);
         let temp;
         if (doc && doc.id) {
             temp = await schema.characterCard.find({
                 id: doc.id
-            }).catch(error => console.error('www #149 mongoDB error: ', error.name, error.reson));
+            });
         }
         let id = [];
         if (doc && doc.channel) {
@@ -167,7 +119,7 @@ io.on('connection', async (socket) => {
         let temp = await schema.characterCard.find(filter);
         socket.emit('getPublicListInfo', {
             temp
-        }).catch(error => console.error('www #170 mongoDB error: ', error.name, error.reson))
+        })
     })
 
     socket.on('publicRolling', async message => {
@@ -211,13 +163,13 @@ io.on('connection', async (socket) => {
                     "channel.id": message.rollTarget.id,
                     "channel.botname": message.rollTarget.botname
                 }
-                let result = await schema.accountPW.findOne(filter).catch(error => console.error('www #214 mongoDB error: ', error.name, error.reson));
+                let result = await schema.accountPW.findOne(filter);
                 if (!result) return;
                 let filter2 = {
                     "botname": message.rollTarget.botname,
                     "id": message.rollTarget.id
                 }
-                let allowRollingResult = await schema.allowRolling.findOne(filter2).catch(error => console.error('www #220 mongoDB error: ', error.name, error.reson));
+                let allowRollingResult = await schema.allowRolling.findOne(filter2);
                 if (!allowRollingResult) return;
                 rplyVal.text = '@' + message.cardName + ' - ' + message.item + '\n' + rplyVal.text;
                 if (message.rollTarget.botname) {
@@ -243,7 +195,7 @@ io.on('connection', async (socket) => {
             userName: message.userName,
             password: SHA(message.userPassword)
         }
-        let doc = await schema.accountPW.findOne(filter).catch(error => console.error('www #246 mongoDB error: ', error.name, error.reson));
+        let doc = await schema.accountPW.findOne(filter);
         let temp;
         if (doc && doc.id) {
             message.card.state = checkNullItem(message.card.state);
@@ -259,7 +211,7 @@ io.on('connection', async (socket) => {
                     roll: message.card.roll,
                     notes: message.card.notes,
                 }
-            }).catch(error => console.error('www #262 mongoDB error: ', error.name, error.reson));
+            });
         }
         if (temp) {
             socket.emit('updateCard', true)
@@ -320,7 +272,7 @@ records.on("new_message", async (message) => {
     if (message.msg && message.name.match(/^HKTRPG/ig)) {
         return;
     }
-
+    
     io.emit(message.roomNumber, message);
     let rplyVal = {}
     var mainMsg = message.msg.match(msgSplitor); // 定義輸入字串
@@ -365,7 +317,7 @@ function checkNullItem(target) {
     });
 }
 async function loadb(io, records, rplyVal, message) {
-    for (let i = 0; i < rplyVal.text.toString().match(/[\s\S]{1,2000}/g).length; i++) {
+    for (var i = 0; i < rplyVal.text.toString().match(/[\s\S]{1,2000}/g).length; i++) {
         io.emit(message.roomNumber, {
             name: 'HKTRPG -> ' + (message.name || 'Sad'),
             msg: rplyVal.text.toString().match(/[\s\S]{1,2000}/g)[i],
@@ -394,15 +346,6 @@ async function limitRaterChatRoom(address) {
 async function limitRaterCard(address) {
     try {
         await rateLimiterCard.consume(address)
-        return false;
-    } catch (error) {
-        return true;
-    }
-}
-
-async function limitRaterApi(address) {
-    try {
-        await rateLimiterApi.consume(address)
         return false;
     } catch (error) {
         return true;
@@ -439,18 +382,4 @@ if (isMaster) {
             });
         }
     });
-}
-
-function createWebServer() {
-    if (!options.key) {
-        server = require('http').createServer(www);
-        console.log('http server');
-    } else {
-        server = require('https').createServer(options, www);
-        console.log('https server');
-    }
-}
-
-function jsonEscape(str) {
-    return str.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t");
 }

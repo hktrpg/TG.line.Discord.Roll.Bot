43d07f73aba304ea3b453ed022d58054b597488a
Lin1009 <76976991+Lin1009@users.noreply.github.com>
2022-03-01 02:11:15 +0800

    Update rollbase.js

--- roll/rollbase.js
+++ roll/rollbase.js
@@ -16,18 +16,18 @@ var gameName = function () {
 }
 
 var gameType = function () {
-  return 'rollbase:hktrpg'
+  return 'dice:rollbase:hktrpg'
 }
 var prefixs = function () {
   const tempregex = /^(?=.*\d+d\d+)(?!.*\d+(l|h))(?!.*(k)$)(?!.*(l|h)(l|h|k|d))(?!.*(k|d)(k|d))(?!.*^[a-z])(?!.*[a-c])(?!.*[e-g])(?!.*[i-j])(?!.*[m-z])(?!.*(([d]|[+]|[-]|[*]|[/])([d]|[+]|[-]|[*]|[/])))(?!.*(^([d]|[+]|[-]|[*]|[/]|[<]|[>]|[=]|[)])))(?!.*([(][)]))(?!.*([<][<]))(?!.*([>][>]))(?!.*([<][>]))(?!.*([>][<]))(?!.*(\d+[d]+\d+[d]([^h|l]))|([)]\d))(?!.*(([d]|[+]|[-]|[*]|[/]|[<]|[>]|[=]|[(])$))(?!.*([@]|[!]|[#]|[$]|[%]|[&]|[_]|[~]|[`]|[']|[?]|\.))(?!.*([\u4e00-\u9fa5]))(?!.*([=].*[=]))(?!.*([+]|[-]|[*]|[/])[=])(?!.*[=]([+]|[-]|[*]|[/]|[>]|[<]))(?!.*(\d)[=](\d))(?!.*([-][>])|([-][<])|([<][-])|([>][-]))(?!.*(d)[(]).*$/ig
   return [{
-      first: tempregex,
-      second: null
-    },
-    {
-      first: /(^[1-9]$)|(^[1-2][0-9]$)|(^[3][0]$)/i,
-      second: tempregex
-    }
+    first: tempregex,
+    second: null
+  },
+  {
+    first: /(^[1-9]$)|(^[1-2][0-9]$)|(^[3][0]$)/i,
+    second: tempregex
+  }
   ]
 }
 
@@ -35,20 +35,20 @@ var prefixs = function () {
 
 ///^(?=.*he)(?!.*da).*$/ig
 const getHelpMessage = function () {
-  return "【基本擲骰】1d100(khN|klN|dhN|dlN)\n\
-例如輸入(2d6+1)*2　攻撃！\n\
-會輸出）(2d6+1)*2：攻撃！  (10[5+5]+1)2 = 22\n\
-如上面一樣,在骰子數字後方隔空白位打字,可以進行發言。\n\
-5 3D6 ：	分別骰出5次3d6 最多30次\n\
-((2d6+1)*2)-5/2>=10 支援括號加減乘除及大於小於(>,<,>=,<=)計算\n\
-支援kh|kl|dh|dl，k keep保留，d drop 放棄，h highest最高，l lowest最低\n\
-如3d6kh 保留最大的1粒骰，3d6dl2 放棄最小的2粒骰"
+  return `【基本擲骰】1d100(khN|klN|dhN|dlN)
+例如輸入(2d6+1)*2  攻撃！
+會輸出）(2d6+1)*2：攻撃！  (10[5+5]+1)2 = 22
+如上面一樣,在骰子數字後方隔空白位打字,可以進行發言。
+5 3D6 ：	分別骰出5次3d6 最多30次
+((2d6+1)*2)-5/2>=10 支援括號加減乘除及大於小於(>,<,>=,<=)計算
+支援kh|kl|dh|dl，k keep保留，d drop 放棄，h highest最高，l lowest最低
+如3d6kh 保留最大的1粒骰，3d6dl2 放棄最小的2粒骰`
 }
 var initialize = function () {
   return variables;
 }
 
-const rollDiceCommand = async function ({
+const rollDiceCommand = function ({
   mainMsg
 }) {
   let rply = {
@@ -56,7 +56,11 @@ const rollDiceCommand = async function ({
     type: 'text',
     text: ''
   };
-  rply.text = await nomalDiceRoller(mainMsg[0], mainMsg[1], mainMsg[2])
+  try {
+    rply.text = nomalDiceRoller(mainMsg[0], mainMsg[1], mainMsg[2]);
+  } catch (error) {
+    rply.text = '';
+  }
   return rply;
 }
 
@@ -67,23 +71,27 @@ const rollDiceCommand = async function ({
  * @param {純數字, 10即骰出1D100} diceSided 
  */
 
-var Dice = async function (diceSided) {
+var Dice = function (diceSided) {
   let result = '';
-  result = await random.integer(1, Math.floor(diceSided))
+  result = random.integer(1, Math.floor(diceSided))
   return result
 }
 
-var DiceINT = async function (start, end) {
+var DiceINT = function (start, end) {
   let result = '';
-  result = await random.integer(start, end)
+  let points = [Math.floor(start), Math.floor(end)]
+  points.sort(function (a, b) {
+    return a - b
+  });
+  result = random.integer(points[0], points[1]);
   return result
 }
 
-var sortNumber = async function (a, b) {
+var sortNumber = function (a, b) {
   return a - b
 }
 
-var RollDice = async function (inputStr) {
+var RollDice = function (inputStr) {
   // 先把inputStr變成字串（不知道為什麼非這樣不可）
   //kh kl dh dl
   //kh or khN Keeps highest N
@@ -99,7 +107,7 @@ var RollDice = async function (inputStr) {
   if (!comStr[1] || !comStr[2]) return;
 
   for (let i = 0; i < comStr[1]; i++) {
-    temp[i] = await Dice(comStr[2])
+    temp[i] = Dice(comStr[2])
     temp2[i] = temp[i]
   }
   if (comStr[3]) {
@@ -156,11 +164,11 @@ var RollDice = async function (inputStr) {
   return finalStr
 }
 
-var FunnyDice = async function (diceSided) {
-  return await random.integer(0, Math.floor(diceSided)) // 猜拳，從0開始
+var FunnyDice = function (diceSided) {
+  return random.integer(0, Math.floor(diceSided)) // 猜拳，從0開始
 }
 
-var BuildDiceCal = async function (inputStr) {
+var BuildDiceCal = function (inputStr) {
   // 首先判斷是否是誤啟動（檢查是否有符合骰子格式）
   if (inputStr.toLowerCase().match(/\d+d\d+/i) == null) return undefined
   // 排除小數點
@@ -178,7 +186,7 @@ var BuildDiceCal = async function (inputStr) {
     //不支援200D以上擲骰
 
     if (tempMatch.toString().split('d')[1] == 1 || tempMatch.toString().split('d')[1] > 500) return;
-    equation = equation.replace(/\d+d\d+/i, await BuildRollDice(tempMatch))
+    equation = equation.replace(/\d+d\d+/i, BuildRollDice(tempMatch))
   }
 
   // 計算算式
@@ -188,17 +196,17 @@ var BuildDiceCal = async function (inputStr) {
   return finalStr
 }
 
-var shuffleTarget = async function (target) {
-  return await random.shuffle(target)
+var shuffleTarget = function (target) {
+  return random.shuffle(target)
 }
 
-var BuildRollDice = async function (inputStr) {
+var BuildRollDice = function (inputStr) {
   // 先把inputStr變成字串（不知道為什麼非這樣不可）
   let comStr = inputStr.toString().toLowerCase()
   let finalStr = '('
 
   for (let i = 1; i <= comStr.split('d')[0]; i++) {
-    finalStr = finalStr + await Dice(comStr.split('d')[1]) + '+'
+    finalStr = finalStr + Dice(comStr.split('d')[1]) + '+'
   }
   finalStr = finalStr.substring(0, finalStr.length - 1) + ')'
   return finalStr
@@ -210,7 +218,7 @@ var BuildRollDice = async function (inputStr) {
  * @param {文字描述 || 1D100} text1 
  * @param {文字描述} text2 
  */
-var nomalDiceRoller = async function (text0, text1, text2) {
+var nomalDiceRoller = function (text0, text1, text2) {
   // 首先判斷是否是誤啟動（檢查是否有符合骰子格式）
   // if (inputStr.toLowerCase().match(/\d+d\d+/) == null) return undefined
   // 再來先把第一個分段拆出來，待會判斷是否是複數擲骰
@@ -228,43 +236,50 @@ var nomalDiceRoller = async function (text0, text1, text2) {
     if (text1.replace(/\d|[+]|[-]|[*]|[/]|[(]|[)]|[d]|[>]|[<]|[=]|[k]|[h]|[l]/ig, '')) return;
     finalStr = text0 + '次擲骰：\n' + text1 + ' ' + (text2 || '') + '\n'
     for (let i = 0; i < mutiOrNot; i++) {
-      finalStr += i + 1 + '# ' + await onetimeroll(text1) + '\n'
+      let answer = onetimeroll(text1)
+      if (answer)
+        finalStr += i + 1 + '# ' + answer + '\n'
+      else return;
     }
   } else {
     if (text0.replace(/\d|[+]|[-]|[*]|[/]|[(]|[)]|[d]|[>]|[<]|[=]|[k]|[h]|[l]/ig, '')) return;
     finalStr = text0 + '：' + (text1 || '') + '\n'
-    finalStr += await onetimeroll(text0)
+    let answer = onetimeroll(text0)
+    if (answer)
+      finalStr += answer;
+    else return;
+
   }
-  return finalStr;
+  return finalStr.replace(/[*]/g, ' * ');
 }
 
 // 單次擲骰
-async function onetimeroll(text0) {
-  let Str = ''
-  //let DiceToRoll = mutiOrNot.toString().toLowerCase()
-  //DiceToRoll = DiceToRoll.toLowerCase()
-  //if (DiceToRoll.match('d') == null) return
-  //if (text0.replace(/\d|[+]|[-]|[*]|[/]|[(]|[)]|[d]|[>]|[<]|[=]|[h]|[k]|[l]/ig, '')) return;
-  //if ((text0.match(/[(]/g) || text0.match(/[)]/g)) && text0.match(/[(]/g).length != text0.match(/[)]/g).length) return;
-  // 寫出算式
-  let equation = text0
-  while (equation.match(regex) != null) {
-    // let totally = 0
-    let tempMatch = equation.match(regex)
-    if (tempMatch[1] > 1000 || tempMatch[1] <= 0) return '不支援零顆以下及一千顆骰以上'
-    if (tempMatch[2] < 1 || tempMatch[2] > 9000000000000000) return '不支援一以下及九千兆以上'
-    equation = equation.replace(regex, await RollDice(tempMatch))
+function onetimeroll(text0) {
+  try {
+    let Str = ''
+    // 寫出算式
+    let equation = text0
+    while (equation.match(regex) != null) {
+      // let totally = 0
+      let tempMatch = equation.match(regex)
+      if (tempMatch[1] > 1000 || tempMatch[1] <= 0) return '不支援零顆以下及一千顆骰以上'
+      if (tempMatch[2] < 1 || tempMatch[2] > 9000000000000000) return '不支援一以下及九千兆以上'
+      equation = equation.replace(regex, RollDice(tempMatch))
+    }
+    // 計算算式
+    let aaa = equation
+    aaa = aaa.replace(/\[.+?\]/ig, '')
+    let answer = math.evaluate(aaa.toString()).toString().replace(/true/i, "成功").replace(/false/i, "失敗");
+    if (equation.match(/[\s\S]{1,250}/g).length > 1) {
+      Str = answer + '（計算過程太長，僅顯示結果）';
+    } else {
+      Str = equation + ' = ' + answer
+    }
+    return Str;
+  } catch (error) {
+    console.error('rollbase error: onetimeroll - inputstr', text0)
+    return '';
   }
-  // 計算算式
-  let aaa = equation
-  aaa = aaa.replace(/\[.+?\]/ig, '')
-  let answer = math.evaluate(aaa.toString()).toString().replace(/true/i, "成功").replace(/false/i, "失敗");
-  if (equation.match(/[\s\S]{1,250}/g).length > 1) {
-    Str = answer + '（計算過程太長，僅顯示結果）';
-  } else {
-    Str = equation + ' = ' + answer
-  }
-  return Str
 }
 module.exports = {
   Dice: Dice,

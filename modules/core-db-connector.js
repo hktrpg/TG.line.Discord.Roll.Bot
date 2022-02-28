1787cb87af8043c8d916f44780cefd8e8ff2d6d0
Lin1009 <76976991+Lin1009@users.noreply.github.com>
2022-03-01 04:49:02 +0800

    Create core-db-connector.js

---
+++ modules/core-db-connector.js
@@ -0,0 +1,33 @@
+"use strict";
+if (!process.env.mongoURL) return;
+const mongoose = require('mongoose');
+/* mongoose.connect(process.env.mongoURL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    });
+*/
+
+(async () => {
+    try {
+        await mongoose.connect(process.env.mongoURL, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+    } catch (err) {
+        console.log('DB CONNECT GET ERROR: ' + err)
+    }
+})()
+
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'mlab connection error:'));
+db.once('open', function () {
+    console.log('mlab  connected!');
+});
+
+module.exports = {
+    mongoose
+};

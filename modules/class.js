cb3951e6dc30fd89105acce4974df215bca2b6b6
Sad <cougar.l@hotmail.com>
2022-04-16 23:13:31 +0800

    Create class.js

---
+++ modules/class.js
@@ -0,0 +1,10 @@
+"use strict";
+class IncomeMessageIdentity {
+    constructor(income) {
+        this.height = height;
+        this.width = width;
+    }
+    identity() {
+        return this;
+    }
+}

3698d4e9982c67d96789e4682ddc57dec6ac5a66
Sad <cougar.l@hotmail.com>
2022-05-09 11:40:40 +0800

    Create edit.js

---
+++ roll/edit.js
@@ -0,0 +1,79 @@
+"use strict";
+if (!process.env.DISCORD_CHANNEL_SECRET) {
+    return;
+}
+const variables = {};
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const gameName = function () {
+    return '【Demo】'
+}
+
+const gameType = function () {
+    return 'Demo:Demo:hktrpg'
+}
+const prefixs = function () {
+    //[mainMSG[0]的prefixs,mainMSG[1]的prefixs,   <---這裡是一對  
+    //mainMSG[0]的prefixs,mainMSG[1]的prefixs  ]  <---這裡是一對
+    //如前面是 /^1$/ig, 後面是/^1D100$/ig, 即 prefixs 變成 1 1D100 
+    ///^(?=.*he)(?!.*da).*$/ig
+    return [{
+        first: /^Demo$/i,
+        second: /^啊$/i
+    }]
+}
+const getHelpMessage = function () {
+    return `【示範】
+只是一個Demo的第一行
+只是一個Demo末行`
+}
+const initialize = function () {
+    return variables;
+}
+
+const rollDiceCommand = async function ({
+    inputStr,
+    mainMsg,
+    groupid,
+    userid,
+    userrole,
+    botname,
+    displayname,
+    channelid,
+    displaynameDiscord,
+    membercount
+}) {
+    let rply = {
+        default: 'on',
+        type: 'text',
+        text: ''
+    };
+    switch (true) {
+        case /^help$/i.test(mainMsg[1]) || !mainMsg[1]: {
+            rply.text = this.getHelpMessage();
+            rply.quotes = true;
+            return rply;
+        }
+        case /^\d+$/i.test(mainMsg[1]): {
+            rply.text = 'Demo' + mainMsg[1] + inputStr + groupid + userid + userrole + botname + displayname + channelid + displaynameDiscord + membercount;
+            return rply;
+        }
+        case /^\S/.test(mainMsg[1] || ''): {
+            rply.text = 'Demo'
+            return rply;
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+const discordCommand = []
+module.exports = {
+    rollDiceCommand,
+    initialize,
+    getHelpMessage,
+    prefixs,
+    gameType,
+    gameName,
+    discordCommand
+};

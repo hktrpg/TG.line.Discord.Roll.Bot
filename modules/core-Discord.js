cd603949001c9e591b13411ebbaf5ccf21e312db
zeteticl <cougar.l@hotmail.com>
2022-06-01 18:52:01 +0800

    x

--- modules/core-Discord.js
+++ modules/core-Discord.js
@@ -4,48 +4,11 @@ if (!process.env.DISCORD_CHANNEL_SECRET) {
 }
 const sharp = require('sharp');
 const Jimp = require('jimp');
-const tokernMaker = async (imageLocation) => {
-    try {
-        const d = new Date();
-        let time = d.getTime();
-        let image = await Jimp.read('./views/image/ONLINE_TOKEN.png')
-        let targetImage = await Jimp.read(imageLocation)
-        targetImage.resize(Jimp.AUTO, 390)
-        if (targetImage.bitmap.width < 378)
-            targetImage.resize(378, Jimp.AUTO)
-        if (targetImage.bitmap.width > 378)
-            targetImage.crop((targetImage.bitmap.width - 378) / 2, 0, 378, targetImage.bitmap.height);
 
 
-        image.composite(targetImage, 70, 27, {
-            mode: Jimp.BLEND_DESTINATION_OVER
-        })
-        const font = await Jimp.loadFont(Jimp.FONT_SANS_128_BLACK);
-        image.print(
-            font,
-            Jimp.HORIZONTAL_ALIGN_CENTER, Jimp.VERTICAL_ALIGN_BOTTOM,
-            {
-                text: '太惡',
-                alignmentX: 0,
-                alignmentY: 0
-            }
-        )
-            .write(`./testImageMaskImage_${time}_${imageLocation.replaceAll('/', '')}.png`);
 
-        return image;
-    } catch (error) {
-        console.log('error', error)
-    }
-}
-tokernMaker('./test/a.png');
 
-tokernMaker('./test/b.png');
-tokernMaker('./test/c.png');
-tokernMaker('./test/d.jpg');
-tokernMaker('./test/f.jpg');
-tokernMaker('./test/g.jpg');
-tokernMaker('./test/h.jpg');
-tokernMaker('./test/i.jpg');
+
 const channelSecret = process.env.DISCORD_CHANNEL_SECRET;
 const Cluster = require('discord-hybrid-sharding');
 require("./ds-deploy-commands");
--- roll/token.js
+++ roll/token.js
@@ -2,7 +2,9 @@
 const variables = {};
 const sharp = require('sharp');
 const Jimp = require('jimp');
+const webp = require('webp-converter');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
 const gameName = function () {
     return '【製作Token】.token'
 }
@@ -40,7 +42,7 @@ const rollDiceCommand = async function ({
     displayname,
     channelid,
     displaynameDiscord,
-    membercount
+    membercount, discordMessage
 }) {
     let rply = {
         default: 'on',
@@ -54,16 +56,25 @@ const rollDiceCommand = async function ({
             return rply;
         }
         case /^\S/.test(mainMsg[1]): {
-            await tokernMaker('./test/a.png');
+            //get avatar  or reply message image
+            console.log('discordClient', discordMessage)
+            const member = await discordMessage.guild.members.fetch(discordMessage.author)
+            //   nickname
+            let nickname = member ? member.displayName : discordMessage.author.username;
+            let avatar = member.displayAvatarURL();
+            console.log('avatar', avatar)
+            console.log('nickname', nickname)
+            const response = await axios.get(encodeURI(avatar), { timeout: 20000 });
+            console.log('response', response)
+            const result = await sharp(response).toFile("./temp/nodejs_logo.png");
 
-            await tokernMaker('./test/b.png');
-            await tokernMaker('./test/c.png');
-            await tokernMaker('./test/d.jpg');
-            await tokernMaker('./test/f.jpg');
-            await tokernMaker('./test/g.jpg');
-            await tokernMaker('./test/h.jpg');
-            await tokernMaker('./test/i.jpg');
-
+            let name = await tokernMaker('./test/a.png');
+            let retries = 5;
+            let success = false;
+            while (retries-- > 0 && !(success = await addTextOnImage(name, mainMsg[1], mainMsg[2]))) {
+                await sleep(500);
+            }
+            //final send the image4
             return rply;
         }
         default: {
@@ -72,10 +83,12 @@ const rollDiceCommand = async function ({
     }
 }
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 const tokernMaker = async (imageLocation) => {
     try {
         const d = new Date();
         let time = d.getTime();
+        let name = `token_${time}_${imageLocation.replaceAll('/', '')}.png`
         let image = await Jimp.read('./views/image/ONLINE_TOKEN.png')
         let targetImage = await Jimp.read(imageLocation)
         targetImage.resize(Jimp.AUTO, 390)
@@ -83,26 +96,51 @@ const tokernMaker = async (imageLocation) => {
             targetImage.resize(378, Jimp.AUTO)
         if (targetImage.bitmap.width > 378)
             targetImage.crop((targetImage.bitmap.width - 378) / 2, 0, 378, targetImage.bitmap.height);
-
-
-        image.composite(targetImage, 70, 27, {
+        await image.composite(targetImage, 70, 27, {
             mode: Jimp.BLEND_DESTINATION_OVER
         })
-        const font = await Jimp.loadFont(Jimp.FONT_SANS_128_BLACK);
-        image.print(
-            font,
-            Jimp.HORIZONTAL_ALIGN_CENTER, Jimp.VERTICAL_ALIGN_BOTTOM,
-            {
-                text: 'Hello world!',
-                alignmentX: 0,
-                alignmentY: 0
-            }
-        )
-            .write(`./testImageMaskImage_${time}_${imageLocation.replaceAll('/', '')}.png`);
-
-        return image;
+            .write(`./temp/temp_${name}`);
+        return name;
     } catch (error) {
         console.log('error', error)
+    }
+}
+
+async function addTextOnImage(name, text, text2) {
+    try {
+        let med = await sharp(`./temp/temp_${name}`).metadata();
+        const width = med.width;
+        const height = med.height;
+        const svgImage = `
+	  <svg width="${width}" height="${height}">
+		<style>
+		.outline {     paint-order: stroke;     stroke: black;     stroke-width: 5px; }
+		.title { fill: #bbafaf; font-size: 62px; font-weight: bold;}
+		.shadow {
+			-webkit-filter: drop-shadow( 3px 3px 2px rgba(0, 0, 0, .7));
+			filter: drop-shadow( 3px 3px 2px rgba(0, 0, 0, .7));
+			/* Similar syntax to box-shadow */
+		  }
+		</style>
+		<text x="50%" y="83%" text-anchor="middle" class="title shadow outline">${text}</text>
+		<text x="50%" y="96%" text-anchor="middle" class="title shadow outline">${text2}</text>
+	  </svg>
+	  `;
+        const svgBuffer = Buffer.from(svgImage);
+        await sharp(`./temp/temp_${name}`)
+            .composite([
+                {
+                    input: svgBuffer,
+                    top: 0,
+                    left: 0,
+                },
+            ])
+            .toFile(`./temp/token_${name}`);
+        return true;
+
+    } catch (error) {
+        // console.log(error);
+        return false;
     }
 }
 

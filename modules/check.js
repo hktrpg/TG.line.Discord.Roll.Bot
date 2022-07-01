91e1144ea4aaf1ab5d82f2c81bf9ee0cb82140d4
zeteticl <cougar.l@hotmail.com>
2022-07-01 14:59:58 +0800

    Update check.js

--- modules/check.js
+++ modules/check.js
@@ -4,29 +4,27 @@ const notAdmin = "你沒有相關權限，禁止使用這功能。\n你需要有
 const notManager = "你沒有相關權限，禁止使用這功能。\n你需要有管理此頻道的權限或群組管理員權限。\n\n";
 const notDiscord = "這是Discord限定功能。\n\n"
 function isChannel(groupid) {
-    if (!groupid) {
-        return notChannel;
-    }
-    return '';
+    if (groupid) return true;
+    else return false;
 }
 function isAdmin(userRole) {
     if (userRole < 3) {
-        return notAdmin;
+        return false;
     }
-    return '';
+    else return true;
 }
 function isManager(userRole) {
     if (userRole < 2) {
-        return notManager;
+        return false;
     }
-    return '';
+    else return true;
 }
 
 function isDiscord(botname) {
-    if (botname !== "Discord") {
-        return notDiscord;
+    if (botname === "Discord") {
+        return true;
     }
-    return '';
+    else return false;
 }
 
 module.exports = {

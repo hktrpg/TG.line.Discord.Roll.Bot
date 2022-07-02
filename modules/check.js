844d613d8fb0d63f26c27e025421a70ab31d0929
BluesLion <mykor.tw@gmail.com>
2022-07-03 01:09:34 +0800

    Refactory check.js and write check.test.js

--- modules/check.js
+++ modules/check.js
@@ -1,30 +1,36 @@
 "use strict";
+
 const notChannel = "這裡不是群組，這是頻道功能，需要在頻道上使用。\n\n";
 const notAdmin = "你沒有相關權限，禁止使用這功能。\n你需要有群組管理員權限。\n\n";
 const notManager = "你沒有相關權限，禁止使用這功能。\n你需要有管理此頻道的權限或群組管理員權限。\n\n";
 const notDiscord = "這是Discord限定功能。\n\n"
-function isChannel(groupid) {
-    if (groupid) return true;
-    else return false;
+
+const role = {
+    ban: -1,
+    nothing: 0,
+    user: 1,
+    dm: 2,
+    admin: 3,
+    superAdmin: 4,
 }
-function isAdmin(userRole) {
-    if (userRole < 3) {
-        return false;
-    }
-    else return true;
+
+function isChannel(gid) {
+    return gid ? true : false;
 }
-function isManager(userRole) {
-    if (userRole < 2) {
-        return false;
-    }
-    else return true;
+
+function isAdmin(user) {
+    return (user == role.admin) ||
+    (user == role.superAdmin) ? true : false;
+}
+
+function isManager(user) {
+    return (user == role.dm) ||
+    (user == role.admin) ||
+    (user == role.superAdmin) ? true : false;
 }
 
 function isDiscord(botname) {
-    if (botname === "Discord") {
-        return true;
-    }
-    else return false;
+    return (botname === "Discord") ? true : false;
 }
 
 module.exports = {
@@ -32,6 +38,7 @@ module.exports = {
     isAdmin,
     isManager,
     isDiscord,
+    role,
     notChannel,
     notAdmin,
     notManager,
---
+++ test/check.test.js
@@ -0,0 +1,65 @@
+const check = require('../modules/check');
+
+test('Test group id is Channel', () => {
+    expect(check.isChannel(5)).toBe(true);
+});
+
+test('Test group id is not Channel', () => {
+    expect(check.isChannel(0)).toBe(false);
+});
+
+test('Test user super admin is admin', () => {
+    expect(check.isAdmin(check.role.superAdmin)).toBe(true);
+});
+
+test('Test user admin is admin', () => {
+    expect(check.isAdmin(check.role.admin)).toBe(true);
+});
+
+test('Test user dm is not admin', () => {
+    expect(check.isAdmin(check.role.dm)).toBe(false);
+});
+
+test('Test user user is not admin', () => {
+    expect(check.isAdmin(check.role.user)).toBe(false);
+});
+
+test('Test user nothing is not admin', () => {
+    expect(check.isAdmin(check.role.nothing)).toBe(false);
+});
+
+test('Test user ban is not admin', () => {
+    expect(check.isAdmin(check.role.ban)).toBe(false);
+});
+
+test('Test user super admin is manager', () => {
+    expect(check.isManager(check.role.superAdmin)).toBe(true);
+});
+
+test('Test user admin is manager', () => {
+    expect(check.isManager(check.role.admin)).toBe(true);
+});
+
+test('Test user dm is not manager', () => {
+    expect(check.isManager(check.role.dm)).toBe(true);
+});
+
+test('Test user user is not manager', () => {
+    expect(check.isManager(check.role.user)).toBe(false);
+});
+
+test('Test user nothing is not manager', () => {
+    expect(check.isManager(check.role.nothing)).toBe(false);
+});
+
+test('Test user ban is not manager', () => {
+    expect(check.isManager(check.role.ban)).toBe(false);
+});
+
+test('Test bot is discord', () => {
+    expect(check.isDiscord('Discord')).toBe(true);
+});
+
+test('Test bot is not discord', () => {
+    expect(check.isDiscord('what ever')).toBe(false);
+});

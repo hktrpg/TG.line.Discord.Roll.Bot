ff7e8d322ed2f9ea1e6179f09f68e9762aa0860a
zeteticl <cougar.l@hotmail.com>
2022-07-10 18:38:07 +0800

    fix empty button

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -727,6 +727,10 @@ async function handlingRequestRollingCharacter(message, input) {
 	}
 	const arrayRow = await splitArray(5, row)
 	for (let index = 0; index < arrayRow.length; index++) {
+		if (arrayRow[index][0].components.length == 0) {
+			await message.reply({ content: `${characterName}的角色卡 沒有技能 \n不能產生Button`, })
+			continue;
+		}
 		try {
 			if (charMode)
 				await message.reply({ content: `${characterName}的角色卡`, components: arrayRow[index] });

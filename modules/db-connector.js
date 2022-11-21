f6e850aaad5c5069a203f40fe5569eb09a4ebf74
zeteticl <cougar.l@hotmail.com>
2022-11-21 13:36:21 +0800

    Update db-connector.js

--- modules/db-connector.js
+++ modules/db-connector.js
@@ -1,29 +1,42 @@
 "use strict";
 if (!process.env.mongoURL) return;
+const reConnectTime = 24 * 60 * 60 * 1000; // One Day
 const master = require.main?.filename.includes('index');
 const mongoose = require('mongoose');
+
 /* mongoose.connect(process.env.mongoURL, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useUnifiedTopology: true
     });
 */
-
-(async () => {
+async function connectMongoDB() {
     try {
         await mongoose.connect(process.env.mongoURL, {
             //    useNewUrlParser: true,
             //  useFindAndModify: false,
             //   useUnifiedTopology: true,
             socketTimeoutMS: 15000,
+            //  poolSize: 10,
             //serverSelectionTimeoutMS: 5000,
 
         });
     } catch (err) {
         console.error('DB CONNECT GET ERROR: ' + err.name, err.reason)
     }
+}
+
+(async () => {
+    connectMongoDB()
+    restartMongoDB();
 })();
 
+function restartMongoDB() {
+    setTimeout(() => {
+        mongoose.connection.close()
+        connectMongoDB();
+    }, reConnectTime);
+}
 
 mongoose.connection.on('error', err => {
     console.error('DB CONNECT ON GET ERROR: ' + err.name, err.reason)

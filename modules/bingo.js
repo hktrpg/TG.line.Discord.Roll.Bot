2a6b81da994e02afd90215feec895b653415c492
Sad <cougar.l@hotmail.com>
2023-01-08 11:35:19 +0800

    x

--- modules/bingo.js
+++ modules/bingo.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+const schema = require('../modules/schema.js');
 class BingoGame {
     constructor(size) {
         this.size = size;
@@ -38,10 +38,58 @@ class BingoGame {
         }
         return score;
     }
-    static(list) {
-        let bingo = [];
-        
+    static async handlingInput(data) {
+        let resultText = '';
+        const { input, buttonlist, gameName, groupID, userID, message, displayname } = data;
+        const achievement = await schema.Achievement.findOne({ groupID, title: gameName[1] });
+        //2.1.1	沒有的話，回覆沒有這個成就，玩不了
+        if (!achievement) return await message.reply({ content: `${gameName} 這個成就好像已被刪除，玩不了啊`, ephemeral: true }).catch();
+        const achievementUser = await schema.AchievementUserScore.findOne({ groupID, userID, title: gameName[1] });
+        if (!achievementUser) {
+            //沒有的話，新增一個User
+            await schema.AchievementUserScore.create({
+                groupID, userID, title: gameName[1], achieved: [input],
+                score: 1
+            });
+            //await message.reply({ content: `恭喜你獲得成就 ${gameName[1]} 的第一個成就點數`, ephemeral: true }).catch();
+            await message.edit({ content: `${messageContent}\n${displayname}獲得1分`}).catch();
+        }
+        else {
+            if (achievementUser.achieved.includes(input)) {
+                //remove achievementUser.achieved input
+                achievementUser.achieved = achievementUser.achieved.filter(item => item !== input)
+                return await message.reply({ content: `${displayname}你已經按過這個了`, ephemeral: true }).catch();
+            }
+            else {
+                achievementUser.achieved.push(input);
+
+            }
+            let score = achievementUser.score + 1;
+            await schema.AchievementUserScore.updateOne({ groupID, userID, title: gameName[1] }, {
+                achieved: achievementUser.achieved, score
+            });
+
+            /**
+             * 1.	確認按了那個
+             * 2.	對比db
+             * 			2.1	 find Achievement 確認是否有這個成就
+             * 					2.1.1	沒有的話，回覆沒有這個成就，玩不了
+             * 					2.1.2	有的話，繼續
+             * 			2.2	 find AchievementUserScore
+             * 						2.2.1	沒有的話，新增一個User
+             * 						2.2.2	對比achieved 和enter，進行開關更新
+             * 						2.2.3	把fullList變成Array [1-X]，然後用checkScore檢查分數
+             * 3.
+             * 4.	以新Message 回覆這個按鈕的結果(隱藏)，但有顯示的按鈕
+             * 5.	更新原本的Message的分數
+             * ----------------------------------
+             * 
+             */
+            return await message.reply({ content: `${displayname}${resultText}`, ephemeral: false }).catch();
+        }
     }
+
+
 }
 
 const bingoThree = new BingoGame(3);
@@ -50,9 +98,7 @@ const bingoFive = new BingoGame(5);
 
 
 module.exports = {
-    bingoThree,
-    bingoFour,
-    bingoFive
+    BingoGame
 };
 
 function checkBingo(bingo, rowLength) {
--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -8,6 +8,7 @@ const Discord = require("discord.js-light");
 const multiServer = require('../modules/multi-server')
 const checkMongodb = require('../modules/dbWatchdog.js');
 const fs = require('node:fs');
+const bingo = require('../modules/bingo').BingoGame;
 const errorCount = [];
 const bingoSize = [9, 16, 25]
 const bingoRowSize = [3, 4, 5]
@@ -1295,50 +1296,9 @@ async function __handlingInteractionMessage(message) {
 						const gameName = messageContent.match(/Bingos遊戲 - (.*)/);
 						const groupID = '0000000000';
 						const userID = message.user.id;
-						const achievement = await schema.Achievement.findOne({ groupID, title: gameName[1] });
-						if (!achievement) return await message.reply({ content: `${gameName} 這個成就好像已被刪除，玩不了啊`, ephemeral: true }).catch();
-						const achievementUser = await schema.AchievementUserScore.findOne({ groupID, userID, title: gameName[1] });
-						if (!achievementUser) {
-							await schema.AchievementUserScore.create({
-								groupID, userID, title: gameName[1], achieved: [input],
-								score: 1
-							});
-							//await message.reply({ content: `恭喜你獲得成就 ${gameName[1]} 的第一個成就點數`, ephemeral: true }).catch();
-							await message.edit({ content: `${messageContent}\n${displayname}獲得成就點數`, components: buttonlist }).catch();
-						}
+						const result = await bingo.handlingInput({ input, buttonlist, gameName, groupID, userID, message, displayname });
 
-						else {
-							if (achievementUser.achieved.includes(input)) {
-								//remove achievementUser.achieved input
-								achievementUser.achieved = achievementUser.achieved.filter(item => item !== input)
-								return await message.reply({ content: `${displayname}你已經按過這個了`, ephemeral: true }).catch();
-							}
-							else {
-								achievementUser.achieved.push(input);
-
-							}
-							let score = achievementUser.score + 1;
-							await schema.AchievementUserScore.updateOne({ groupID, userID, title: gameName[1] }, {
-								achieved: achievementUser.achieved, score
-							});
-							/**
-							 * 1.	確認按了那個
-							 * 2.	對比db
-							 * 			2.1	 find Achievement 確認是否有這個成就
-							 * 					2.1.1	沒有的話，回覆沒有這個成就，玩不了
-							 * 					2.1.2	有的話，繼續
-							 * 			2.2	 find AchievementUserScore
-							 * 						2.2.1	沒有的話，新增一個User
-							 * 						2.2.2	對比achieved 和enter，進行開關更新
-							 * 						2.2.3	把fullList變成Array [1-X]，然後用checkScore檢查分數
-							 * 3.
-							 * 4.	以新Message 回覆這個按鈕的結果(隱藏)，但有顯示的按鈕
-							 * 5.	更新原本的Message的分數
-							 * ----------------------------------
-							 * 
-							 */
-							return await message.reply({ content: `${displayname}${resultText}`, ephemeral: false }).catch();
-						}
+						return;
 					} catch (error) {
 						console.error('Bingo遊戲error', error)
 					}

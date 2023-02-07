8ebd145af8c3b19f5266f96f407a329be61c1d13
Sad <cougar.l@hotmail.com>
2023-02-08 01:34:37 +0800

    vs function

--- roll/pokemon.js
+++ roll/pokemon.js
@@ -30,7 +30,7 @@ const prefixs = function () {
     //如前面是 /^1$/ig, 後面是/^1D100$/ig, 即 prefixs 變成 1 1D100 
     ///^(?=.*he)(?!.*da).*$/ig
     return [{
-        first: /^poke$/i,
+        first: /^\.poke$/i,
         second: null
     }]
 }
@@ -67,7 +67,72 @@ const rollDiceCommand = async function ({
             rply.button = ['']
             return rply;
         }
-        case /^\S/.test(mainMsg[1] || ''): {
+        case /^vs$/.test(mainMsg[1] || ''): {
+            try {
+                console.log('WW')
+                //招式名,屬性  VS  POKEMON名,POKEMON NO,屬性1,屬性2
+                let attackerType = Moves.findKeyByValue(mainMsg[2]);
+                console.log('attackerType', attackerType)
+                let attacker = pokeMove.getVS(mainMsg[2]);
+                if (!attackerType && attacker) {
+                    attackerType = attacker.type
+                }
+                let defenderType = Pokemon.findKeyByValue(mainMsg[3]);
+                let defender = pokedex.getVS(mainMsg[3]);
+                if (!defenderType.length && defender) {
+                    defenderType = defender.type
+                }
+
+                console.log('defenderType', defenderType, defender)
+                if (mainMsg[4]) {
+                    let defenderType2 = Pokemon.findKeyByValue(mainMsg[4]);
+                    console.log('defenderType2', defenderType2)
+                    if (defenderType2) defenderType = defenderType.concat(defenderType2);
+                }
+                if (!defenderType || !attackerType) {
+                    rply.text += (!attackerType) ? '找不到攻方屬性，請確認名稱，你可以輸入完整招式名稱或屬性\n' : '';
+                    rply.text += (!defenderType) ? '找不到防方屬性，請確認名稱，你可以輸入小精靈名稱，編號或屬性\n' : '';
+                    return rply;
+
+                }
+                console.log('attacker', attacker, attackerType)
+                console.log('defender', defender, defenderType)
+                let typeEffect = checkEffectiveness(attackerType, defenderType);
+                /**
+                 * 攻方屬性：attackerType
+                 * 防方屬性：defenderType
+                 * 屬性效果：typeEffect.script
+                 * --------------------
+                 * 攻方招式：attacker.name
+                 * 攻方招式內容：attacker.effect desc
+                 * 攻方招式傷害：attacker.damage
+                 * --------------------
+                 * 防方小精靈：defender.name
+                 * 防方小精靈圖片：defender.info.image
+                 */
+                rply.text +=
+                    `攻方屬性：${attackerType}
+防方屬性：${defenderType}
+屬性效果：${typeEffect.script}
+`
+                rply.text += (attacker) ?
+                    `--------------------
+攻方招式：${attacker.name}
+攻方招式內容：${attacker.effect}
+${attacker.desc}
+攻方招式傷害：${attacker.damage}
+`: '';
+                rply.text += (defender) ?
+                    `--------------------
+防方小精靈：${defender.name}
+防方小精靈圖片：${defender.info.image}
+`: '';
+                return rply;
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        case /^sh$/.test(mainMsg[1] || ''): {
             rply.text = 'Demo'
             return rply;
         }
@@ -77,39 +142,53 @@ const rollDiceCommand = async function ({
     }
 }
 
-
 class Pokemon {
     constructor(data) {
         this.pokemonData = data;
         this.fuse = new Fuse(this.pokemonData, {
-            keys: ['name'],
+            keys: ['name', 'id', 'alias'],
             includeScore: true,
-            threshold: 0.3
+            findAllMatches: true,
+            threshold: 0
         });
     }
 
-    static init() {
+    static init(link) {
         let data = [];
-        for (let i = 0; i < datalink.length; i++) {
-            let temp = require(datalink[i]);
-            data = data.concat(Pokemon.objectToArray(temp.helpdoc))
-        }
-
+        console.log('init')
+        require('fs').readdirSync('./assets/pokemon/').forEach(function (file) {
+            if (file.match(/\.js$/) && file.match(new RegExp('^' + link, 'i'))) {
+                //   console.log('file', file)
+                let importData = require('../assets/pokemon/' + file);
+                //   console.log('importData', importData)
+                data = data.concat(importData.Pokedex)
+            }
+        });
+        console.log('data', data[0])
         return new Pokemon(data);
     }
-    static objectToArray(input) {
-        let data = [];
-        for (let i = 0; i < Object.keys(input).length; i++) {
-            data.push({
-                name: Object.keys(input)[i],
-                desc: Object.values(input)[i]
-            });
+    getVS(string) {
+        /**
+         * 
+         */
+        if (typeof (string) === 'number') { string = ('000' + string).slice(-3) }
+        let result = this.fuse.search(string, { limit: 1 })
+        console.log('result1', result)
+        if (result.length) return result[0].item;
+        return undefined;
+    }
+    static findKeyByValue(value) {
+        for (const key in typeName) {
+            if (typeName[key] === value) {
+                return [key];
+            }
         }
-        return data;
+        return [];
     }
     search(name) {
         try {
-            let result = this.fuse.search(name);
+            let result = this.fuse.search(name, { findAllMatches: true, limit: 5 });
+            console.log('search:\n', result)
             let rply = '';
             if (result.length === 0) return '沒有找到相關資料';
             if (result[0].item.name === name) {
@@ -138,8 +217,85 @@ ${result[i].item.desc} \n
         }
     }
 }
-const pokedex = Pokemon.init('../assets/pokedex-');
-const pokeMove = Pokemon.init('../assets/moves-');
+
+
+class Moves {
+    constructor(data) {
+        this.pokemonData = data;
+        this.fuse = new Fuse(this.pokemonData, {
+            keys: ['name', 'id', 'alias'],
+            includeScore: true,
+            findAllMatches: true,
+            threshold: 0
+        });
+    }
+
+    static init(link) {
+        let data = [];
+        console.log('initMoves')
+        require('fs').readdirSync('./assets/pokemon/').forEach(function (file) {
+            if (file.match(/\.js$/) && file.match(new RegExp('^' + link, 'i'))) {
+                //   console.log('file', file)
+                let importData = require('../assets/pokemon/' + file);
+                //console.log('importData', importData)
+                data = data.concat(importData.MoveList)
+            }
+        });
+        console.log('data', data[0])
+        return new Pokemon(data);
+    }
+    getVS(string) {
+        /**
+         * 
+         */
+        if (typeof (string) === 'number') { string = ('000' + string).slice(-3) }
+        let result = this.fuse.search(string, { limit: 1 })
+        console.log('result1', result)
+        if (result)
+            return result[0].item;
+    }
+    static findKeyByValue(value) {
+        for (const key in typeName) {
+            if (typeName[key] === value) {
+                return key;
+            }
+        }
+        return undefined;
+    }
+    search(name) {
+        try {
+            let result = this.fuse.search(name, { findAllMatches: true, limit: 5 });
+            console.log('search:\n', result)
+            let rply = '';
+            if (result.length === 0) return '沒有找到相關資料';
+            if (result[0].item.name === name) {
+                return `【${result[0].item.name}】
+        ${result[0].item.desc} \n
+         `;
+            }
+            if (result.length <= 2) {
+                for (let i = 0; i < result.length; i++) {
+                    rply += `【${result[i].item.name}】
+${result[i].item.desc} \n
+ `;
+                }
+            }
+            else {
+                rply += '找到太多相關資料，請更精確的查詢\n\n';
+                for (let i = 0; i < result.length; i++) {
+                    rply += `${result[i].item.name}\n`;
+                }
+            }
+            return rply;
+        }
+        catch (error) {
+            console.log(error);
+            return '發生錯誤';
+        }
+    }
+}
+const pokedex = Pokemon.init('pokedex-');
+const pokeMove = Moves.init('moves-');
 /**
  * 無效 = 0 = -999 
  * 弱效 = 1 = -1
@@ -147,25 +303,29 @@ const pokeMove = Pokemon.init('../assets/moves-');
  * 克制 = 3 = 1
  */
 
+const typeName = {
+    Normal: '一般', Fighting: '格鬥', Flying: '飛行', Poison: '毒', Ground: '地面', Rock: '岩石', Bug: '蟲', Ghost: '幽靈', Steel: '鋼', Fire: '火', Water: '水', Grass: '草', Electric: '電', Psychic: '超能力', Ice: '冰', Dragon: '龍', Dark: '惡', Fairy: '妖精'
+}
+
 const typeChart = {
-    normal: { normal: 0, fighting: 0, flying: 0, poison: 0, ground: 0, rock: -1, bug: 0, ghost: -999, steel: -1, fire: 0, water: 0, grass: 0, electric: 0, psychic: 0, ice: 0, dragon: 0, dark: 0, fairy: 0 },
-    fighting: { normal: 1, fighting: 0, flying: -1, poison: -1, ground: 0, rock: 1, bug: -1, ghost: -999, steel: 1, fire: 0, water: 0, grass: 0, electric: 0, psychic: -1, ice: 1, dragon: 0, dark: 1, fairy: -1 },
-    flying: { normal: 0, fighting: 1, flying: 0, poison: 0, ground: 0, rock: -1, bug: 1, ghost: 0, steel: -1, fire: 0, water: 0, grass: 1, electric: -1, psychic: 0, ice: 0, dragon: 0, dark: 0, fairy: 0 },
-    poison: { normal: 0, fighting: 0, flying: 0, poison: -1, ground: -999, rock: -1, bug: 0, ghost: -1, steel: -999, fire: 0, water: 0, grass: 1, electric: 0, psychic: 0, ice: 0, dragon: 0, dark: 0, fairy: 1 },
-    ground: { normal: 0, fighting: 0, flying: -999, poison: 1, ground: 0, rock: 1, bug: -1, ghost: 0, steel: 1, fire: 1, water: 0, grass: -1, electric: 1, psychic: 0, ice: 0, dragon: 0, dark: 0, fairy: 0 },
-    rock: { normal: 0, fighting: -1, flying: 1, poison: 0, ground: -1, rock: 0, bug: 1, ghost: 0, steel: -1, fire: 1, water: 0, grass: 0, electric: 0, psychic: 0, ice: 1, dragon: 0, dark: 0, fairy: 0 },
-    bug: { normal: 0, fighting: -1, flying: -1, poison: -1, ground: 0, rock: 0, bug: 0, ghost: -1, steel: -1, fire: -1, water: 0, grass: 1, electric: 0, psychic: 1, ice: 0, dragon: 0, dark: 1, fairy: -1 },
-    ghost: { normal: -999, fighting: 0, flying: 0, poison: 0, ground: 0, rock: 0, bug: 0, ghost: 1, steel: 0, fire: 0, water: 0, grass: 0, electric: 0, psychic: 1, ice: 0, dragon: 0, dark: -1, fairy: 0 },
-    steel: { normal: 0, fighting: 0, flying: 0, poison: 0, ground: 0, rock: 1, bug: 0, ghost: 0, steel: -1, fire: -1, water: -1, grass: 0, electric: -1, psychic: 0, ice: 1, dragon: 0, dark: 0, fairy: 1 },
-    fire: { normal: 0, fighting: 0, flying: 0, poison: 0, ground: 0, rock: -1, bug: 1, ghost: 0, steel: 1, fire: -1, water: -1, grass: 1, electric: 0, psychic: 0, ice: 1, dragon: -1, dark: 0, fairy: 0 },
-    water: { normal: 0, fighting: 0, flying: 0, poison: 0, ground: 1, rock: 1, bug: 0, ghost: 0, steel: 0, fire: 1, water: -1, grass: -1, electric: 0, psychic: 0, ice: 0, dragon: -1, dark: 0, fairy: 0 },
-    grass: { normal: 0, fighting: 0, flying: -1, poison: -1, ground: 1, rock: 1, bug: -1, ghost: 0, steel: -1, fire: -1, water: 1, grass: -1, electric: 0, psychic: 0, ice: 0, dragon: -1, dark: 0, fairy: 0 },
-    electric: { normal: 0, fighting: 0, flying: 1, poison: 0, ground: -999, rock: 0, bug: 0, ghost: 0, steel: 0, fire: 0, water: 1, grass: -1, electric: -1, psychic: 0, ice: 0, dragon: -1, dark: 0, fairy: 0 },
-    psychic: { normal: 0, fighting: 1, flying: 0, poison: 1, ground: 0, rock: 0, bug: 0, ghost: 0, steel: -1, fire: 0, water: 0, grass: 0, electric: 0, psychic: -1, ice: 0, dragon: 0, dark: -999, fairy: 0 },
-    ice: { normal: 0, fighting: 0, flying: 1, poison: 0, ground: 1, rock: 0, bug: 0, ghost: 0, steel: 1, fire: -1, water: -1, grass: 1, electric: 0, psychic: 0, ice: -1, dragon: 1, dark: 0, fairy: 0 },
-    dragon: { normal: 0, fighting: 0, flying: 0, poison: 0, ground: 0, rock: 0, bug: 0, ghost: 0, steel: -1, fire: 0, water: 0, grass: 0, electric: 0, psychic: 0, ice: 0, dragon: 1, dark: 0, fairy: -999 },
-    dark: { normal: 0, fighting: -1, flying: 0, poison: 0, ground: 0, rock: 0, bug: 0, ghost: 1, steel: 0, fire: 0, water: 0, grass: 0, electric: 0, psychic: 1, ice: 0, dragon: 0, dark: -1, fairy: -1 },
-    fairy: { normal: 0, fighting: 1, flying: 0, poison: -1, ground: 0, rock: 0, bug: 0, ghost: 0, steel: -1, fire: -1, water: 0, grass: 0, electric: 0, psychic: 0, ice: 0, dragon: 1, dark: 1, fairy: 0 }
+    Normal: { Normal: 0, Fighting: 0, Flying: 0, Poison: 0, Ground: 0, Rock: -1, Bug: 0, Ghost: -999, Steel: -1, Fire: 0, Water: 0, Grass: 0, Electric: 0, Psychic: 0, Ice: 0, Dragon: 0, Dark: 0, Fairy: 0 },
+    Fighting: { Normal: 1, Fighting: 0, Flying: -1, Poison: -1, Ground: 0, Rock: 1, Bug: -1, Ghost: -999, Steel: 1, Fire: 0, Water: 0, Grass: 0, Electric: 0, Psychic: -1, Ice: 1, Dragon: 0, Dark: 1, Fairy: -1 },
+    Flying: { Normal: 0, Fighting: 1, Flying: 0, Poison: 0, Ground: 0, Rock: -1, Bug: 1, Ghost: 0, Steel: -1, Fire: 0, Water: 0, Grass: 1, Electric: -1, Psychic: 0, Ice: 0, Dragon: 0, Dark: 0, Fairy: 0 },
+    Poison: { Normal: 0, Fighting: 0, Flying: 0, Poison: -1, Ground: -999, Rock: -1, Bug: 0, Ghost: -1, Steel: -999, Fire: 0, Water: 0, Grass: 1, Electric: 0, Psychic: 0, Ice: 0, Dragon: 0, Dark: 0, Fairy: 1 },
+    Ground: { Normal: 0, Fighting: 0, Flying: -999, Poison: 1, Ground: 0, Rock: 1, Bug: -1, Ghost: 0, Steel: 1, Fire: 1, Water: 0, Grass: -1, Electric: 1, Psychic: 0, Ice: 0, Dragon: 0, Dark: 0, Fairy: 0 },
+    Rock: { Normal: 0, Fighting: -1, Flying: 1, Poison: 0, Ground: -1, Rock: 0, Bug: 1, Ghost: 0, Steel: -1, Fire: 1, Water: 0, Grass: 0, Electric: 0, Psychic: 0, Ice: 1, Dragon: 0, Dark: 0, Fairy: 0 },
+    Bug: { Normal: 0, Fighting: -1, Flying: -1, Poison: -1, Ground: 0, Rock: 0, Bug: 0, Ghost: -1, Steel: -1, Fire: -1, Water: 0, Grass: 1, Electric: 0, Psychic: 1, Ice: 0, Dragon: 0, Dark: 1, Fairy: -1 },
+    Ghost: { Normal: -999, Fighting: 0, Flying: 0, Poison: 0, Ground: 0, Rock: 0, Bug: 0, Ghost: 1, Steel: 0, Fire: 0, Water: 0, Grass: 0, Electric: 0, Psychic: 1, Ice: 0, Dragon: 0, Dark: -1, Fairy: 0 },
+    Steel: { Normal: 0, Fighting: 0, Flying: 0, Poison: 0, Ground: 0, Rock: 1, Bug: 0, Ghost: 0, Steel: -1, Fire: -1, Water: -1, Grass: 0, Electric: -1, Psychic: 0, Ice: 1, Dragon: 0, Dark: 0, Fairy: 1 },
+    Fire: { Normal: 0, Fighting: 0, Flying: 0, Poison: 0, Ground: 0, Rock: -1, Bug: 1, Ghost: 0, Steel: 1, Fire: -1, Water: -1, Grass: 1, Electric: 0, Psychic: 0, Ice: 1, Dragon: -1, Dark: 0, Fairy: 0 },
+    Water: { Normal: 0, Fighting: 0, Flying: 0, Poison: 0, Ground: 1, Rock: 1, Bug: 0, Ghost: 0, Steel: 0, Fire: 1, Water: -1, Grass: -1, Electric: 0, Psychic: 0, Ice: 0, Dragon: -1, Dark: 0, Fairy: 0 },
+    Grass: { Normal: 0, Fighting: 0, Flying: -1, Poison: -1, Ground: 1, Rock: 1, Bug: -1, Ghost: 0, Steel: -1, Fire: -1, Water: 1, Grass: -1, Electric: 0, Psychic: 0, Ice: 0, Dragon: -1, Dark: 0, Fairy: 0 },
+    Electric: { Normal: 0, Fighting: 0, Flying: 1, Poison: 0, Ground: -999, Rock: 0, Bug: 0, Ghost: 0, Steel: 0, Fire: 0, Water: 1, Grass: -1, Electric: -1, Psychic: 0, Ice: 0, Dragon: -1, Dark: 0, Fairy: 0 },
+    Psychic: { Normal: 0, Fighting: 1, Flying: 0, Poison: 1, Ground: 0, Rock: 0, Bug: 0, Ghost: 0, Steel: -1, Fire: 0, Water: 0, Grass: 0, Electric: 0, Psychic: -1, Ice: 0, Dragon: 0, Dark: -999, Fairy: 0 },
+    Ice: { Normal: 0, Fighting: 0, Flying: 1, Poison: 0, Ground: 1, Rock: 0, Bug: 0, Ghost: 0, Steel: 1, Fire: -1, Water: -1, Grass: 1, Electric: 0, Psychic: 0, Ice: -1, Dragon: 1, Dark: 0, Fairy: 0 },
+    Dragon: { Normal: 0, Fighting: 0, Flying: 0, Poison: 0, Ground: 0, Rock: 0, Bug: 0, Ghost: 0, Steel: -1, Fire: 0, Water: 0, Grass: 0, Electric: 0, Psychic: 0, Ice: 0, Dragon: 1, Dark: 0, Fairy: -999 },
+    Dark: { Normal: 0, Fighting: -1, Flying: 0, Poison: 0, Ground: 0, Rock: 0, Bug: 0, Ghost: 1, Steel: 0, Fire: 0, Water: 0, Grass: 0, Electric: 0, Psychic: 1, Ice: 0, Dragon: 0, Dark: -1, Fairy: -1 },
+    Fairy: { Normal: 0, Fighting: 1, Flying: 0, Poison: -1, Ground: 0, Rock: 0, Bug: 0, Ghost: 0, Steel: -1, Fire: -1, Water: 0, Grass: 0, Electric: 0, Psychic: 0, Ice: 0, Dragon: 1, Dark: 1, Fairy: 0 }
 };
 
 const effect = {
@@ -177,11 +337,10 @@ const effect = {
 
 }
 // 定義函式
-function checkEffectiveness(moveType, enemyType1, enemyType2) {
+function checkEffectiveness(moveType, enemyType) {
     /**
      * @param {string} moveType - 技能的屬性
-     * @param {string} enemyType1 - 敵人的第一個屬性
-     * @param {string} enemyType2 - 敵人的第二個屬性
+     * @param {Array} enemyType - 敵人的兩個屬性
      * @return {number} effectiveness - 技能的威力
      * 
      * @example
@@ -190,6 +349,8 @@ function checkEffectiveness(moveType, enemyType1, enemyType2) {
      * effectiveness = 1,2 表示技能的威力為 效果絕佳
      * effectiveness = -1,-2 表示技能的威力為 效果絕佳
      */
+    let enemyType1 = enemyType[0];
+    let enemyType2 = enemyType[1];
     let effectiveness = 0;
     let level = typeChart[moveType][enemyType1];
     if (level == -999) return { effect: -999, script: "免疫該攻擊傷害" };
@@ -206,9 +367,9 @@ function checkEffectiveness(moveType, enemyType1, enemyType2) {
 
 
 // 測試程式
-console.log(checkEffectiveness("fire", "grass", "ice")); // 輸出 0.25
-console.log(checkEffectiveness("electric", "water")); // 輸出 2
-console.log(checkEffectiveness("poison", "steel")); // 輸出 0
+console.log(checkEffectiveness("Fire", ["Grass", "Ice"])); // 輸出 0.25
+console.log(checkEffectiveness("Electric", ["Water"])); // 輸出 2
+console.log(checkEffectiveness("Poison", ["Steel"])); // 輸出 0
 
 const discordCommand = []
 module.exports = {

76d843749d6a7156d1139a29fb87c7ee892590f9
Sad <cougar.l@hotmail.com>
2023-04-27 01:38:51 +0800

    try

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ process.on('warning', (warning) => {
   console.warn('warning', warning.message); // Print the warning message
   console.warn('warning', warning.stack); // Print the stack trace
 });
+
+process.stdout.on('error', function (err) {
+  if (err.code == "EPIPE") {
+    console.log('EPIPE err:', err);
+  }
+});
 /*
 流程解釋
 

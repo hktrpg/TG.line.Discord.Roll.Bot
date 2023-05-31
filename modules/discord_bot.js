f85262ec64f5f489b4cf8619b2065cd1a3da674e
zeteticl <cougar.l@hotmail.com>
2023-05-31 18:45:43 +0800

    Update discord_bot.js

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -1302,14 +1302,14 @@ async function __handlingInteractionMessage(message) {
 				const displayname = (message.member && message.member.id) ? `<@${message.member.id}>\n` : '';
 				const resultText = (result && result.text) || '';
 				if (/的角色卡$/.test(messageContent)) {
-					if (resultText) { return await message.reply({ content: `${displayname}${messageContent.replace(/的角色卡$/, '')}進行擲骰 \n${resultText}`, ephemeral: false }).catch() }
+					if (resultText) { return await message.deferReply({ content: `${displayname}${messageContent.replace(/的角色卡$/, '')}進行擲骰 \n${resultText}`, ephemeral: false }).catch() }
 					else {
-						return await message.reply({ content: `${displayname}沒有反應，請檢查按鈕內容`, ephemeral: true }).catch()
+						return await message.deferReply({ content: `${displayname}沒有反應，請檢查按鈕內容`, ephemeral: true }).catch()
 					}
 				}
 				if (/的角色$/.test(messageContent)) {
 					try {
-						return await message.reply({ content: `${displayname}${resultText}`, ephemeral: false })
+						return await message.deferReply({ content: `${displayname}${resultText}`, ephemeral: false })
 					} catch (error) {
 						null;
 					}

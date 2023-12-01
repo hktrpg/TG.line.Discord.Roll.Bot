de5b2d0260ae82604ee8b2d0130b9fbb65e38161
Sad <cougar.l@hotmail.com>
2023-12-01 12:00:33 +0800

    use :path

--- modules/core-www.js
+++ modules/core-www.js
@@ -142,10 +142,10 @@ www.get('/api', async (req, res) => {
 });
 
 // 將/publiccard/css/設置為靜態資源的路徑
-www.use('/publiccard/css/', express.static(process.cwd() + '/views/css/'));
+www.use('/:path/css/', express.static(process.cwd() + '/views/css/'));
 
 // 將/publiccard/includes/設置為靜態資源的路徑
-www.use('/publiccard/includes/', express.static(process.cwd() + '/views/includes/'));
+www.use('/:path/includes/', express.static(process.cwd() + '/views/includes/'));
 
 
 www.get('/card', (req, res) => {

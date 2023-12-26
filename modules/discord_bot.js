dcbfb7aa828914d1661051f60a894d376eb22311
Sad <cougar.l@hotmail.com>
2023-12-26 12:45:41 +0800

    test mode

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -108,6 +108,7 @@ const WebSocket = require('ws');
 let ws;
 
 client.on('messageCreate', async message => {
+	return;
 	try {
 		if (message.author.bot) return;
 		if (!checkMongodb.isDbOnline() && checkMongodb.isDbRespawn()) {
@@ -125,6 +126,7 @@ client.on('messageCreate', async message => {
 
 });
 client.on('guildCreate', async guild => {
+	return;
 	try {
 		const channels = await guild.channels.fetch();
 		const keys = Array.from(channels.values());
@@ -148,6 +150,7 @@ client.on('guildCreate', async guild => {
 })
 
 client.on('interactionCreate', async message => {
+	return;
 	try {
 		if (message.user && message.user.bot) return;
 		return __handlingInteractionMessage(message);
@@ -158,6 +161,7 @@ client.on('interactionCreate', async message => {
 
 
 client.on('messageReactionAdd', async (reaction, user) => {
+	return;
 	if (!checkMongodb.isDbOnline()) return;
 	if (reaction.me) return;
 	const list = await schema.roleReact.findOne({ messageID: reaction.message.id, groupid: reaction.message.guildId })
@@ -185,6 +189,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 });
 
 client.on('messageReactionRemove', async (reaction, user) => {
+	return;
 	if (!checkMongodb.isDbOnline()) return;
 	if (reaction.me) return;
 	const list = await schema.roleReact.findOne({ messageID: reaction.message.id, groupid: reaction.message.guildId }).catch(error => console.error('discord_bot #817 mongoDB error: ', error.name, error.reson))
@@ -211,6 +216,7 @@ const sleep = async (minutes) => {
 
 
 client.once('ready', async () => {
+	return;
 	initInteractionCommands();
 	if (process.env.BROADCAST) connect();
 	//	if (shardid === 0) getSchedule();

fe3ee1c2fc06d1bd8c0a65285569b22686a6bb20
Sad <cougar.l@hotmail.com>
2023-12-27 01:20:42 +0800

    update package and DISCORDDELAY

--- modules/core-Discord.js
+++ modules/core-Discord.js
@@ -2,7 +2,7 @@
 if (!process.env.DISCORD_CHANNEL_SECRET) {
 	return;
 }
-const DELAY = process.env.DISCORDDELAY || 1000 * 5;
+const DELAY = process.env.DISCORDDELAY || 1000 * 15;
 const agenda = require('../modules/schedule') && require('../modules/schedule').agenda;
 const channelSecret = process.env.DISCORD_CHANNEL_SECRET;
 const { ClusterManager, HeartbeatManager } = require('discord-hybrid-sharding');

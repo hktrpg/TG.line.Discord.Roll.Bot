2f0cde6a82d1c43fbc80a16cdae114f9b4649b02
Sad <cougar.l@hotmail.com>
2023-12-26 16:35:45 +0800

    manager.spawn with delay 20sec

--- modules/core-Discord.js
+++ modules/core-Discord.js
@@ -71,5 +71,5 @@ manager.extend(
 	})
 )
 
-manager.spawn({ timeout: -1 });
+manager.spawn({ timeout: -1, delay: 1000 * 20 });
 //manager.recluster?.start({ restartMode: 'gracefulSwitch', totalShards: getInfo().TOTAL_SHARDS })

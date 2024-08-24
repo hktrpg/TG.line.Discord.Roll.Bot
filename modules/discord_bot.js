c3915e6e1a9415d6937a1145015817a30942c367
Sad <cougar.l@hotmail.com>
2024-08-24 23:00:46 +0800

    fix 所有啓動中的shard ID

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -809,7 +809,7 @@ async function getAllshardIds() {
 	return Promise.all(promises)
 		.then(results => {
 			return `\n現在的shard ID: ${results[3]}
-			所有啓動中的shard ID:   ${results[0]} 
+			所有啓動中的shard ID:   ${results[0].join(", ")} 
 			所有啓動中的shard online:   ${results[1].map(ele => discordPresenceStatus[ele]).join(', ').replace(/online/g, '在線')} 
 			所有啓動中的shard ping:   ${results[2].map(ele => ele.toFixed(0)).join(', ')}`
 		})

3b4a981083ec96ff51675a1bcf201edc450176ef
Sad <cougar.l@hotmail.com>
2024-12-31 02:55:14 +0800

    Update getRoll.js

--- modules/getRoll.js
+++ modules/getRoll.js
@@ -1,28 +1,27 @@
-
 const rollDice = require('./analytics').parseInput;
 
+// Function to roll text with embedded dice roll expressions
 async function rollText(text) {
-    let result = await replaceAsync(text, /\[\[(.*?)\]\]/ig, await myAsyncFn);
+    // Replace all instances of [[expression]] with the result of the dice roll
+    let result = await replaceAsync(text, /\[\[(.*?)\]\]/ig, rollDiceExpression);
     return result;
 }
 
-
+// Helper function to replace regex matches with asynchronous function results
 async function replaceAsync(str, regex, asyncFn) {
     const promises = [];
     str.replace(regex, (match, ...args) => {
-        const promise = asyncFn(match, ...args);
-        promises.push(promise);
+        promises.push(asyncFn(match, ...args));
     });
     const data = await Promise.all(promises);
     return str.replace(regex, () => data.shift());
 }
 
-
-async function myAsyncFn(match, p1) {
-    let result = await rollDice({ inputStr: p1 });
+// Asynchronous function to process each dice roll expression
+async function rollDiceExpression(match, expression) {
+    let result = await rollDice({ inputStr: expression });
     return (result && result.text) ? result.text : match;
 }
-
 
 module.exports = {
     rollText
